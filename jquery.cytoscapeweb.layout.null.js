0dcb9450ec1fc209710a580ac67568bab9e6fdc7
Max Franz <maxkfranz@gmail.com>
2012-01-12 18:10:36 -0500

    Fix nul layout cy reference

--- jquery.cytoscapeweb.layout.null.js
+++ jquery.cytoscapeweb.layout.null.js
@@ -7,7 +7,10 @@ $(function(){
 	// puts all nodes at (0, 0)
 	NullLayout.prototype.run = function(params){
 		$.cytoscapeweb("debug", "Running null layout with options (%o)", params);
-		params.cy.nodes().positions(function(){
+		
+		var cy = params.cy;
+		
+		cy.nodes().positions(function(){
 			return {
 				x: 0,
 				y: 0

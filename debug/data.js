ea382b4ba34b7eacd82fd6da389b94cd36247fb5
Max Franz <maxkfranz@gmail.com>
2012-03-23 17:43:13 -0400

    Remove qtips in debug page; use gritter

--- debug/data.js
+++ debug/data.js
@@ -4,43 +4,23 @@ $(function(){
 		var cy = e.cy;
 		
 		function displayElementData(element, position){
-			var content = $('<div></div>');
+			var content = '';
 			var data = element.data();
 			
 			$.each(data, function(name, value){
-				content.append('<p><em>' + name + '</em> = ' + value + '</p>');
+				content += ('<p><code><strong>' + name + '</strong></code> = ' + value + '</p>');
 			});
 			
-			$("body").commandtip({
-				paper: false,
-				position: {
-					x: position.x,
-					y: position.y
-				},
-				content: content
+			$.gritter.add({
+				title: 'Data for ' + element.data("id"),
+				text: content
 			});
 		}
 		
-		cy.nodes().live("click", function(e){
+		cy.$("node, edge").live("click", function(e){
 			if( e.metaKey ){
-				displayElementData(this, {
-					x: $("#cytoscapeweb").offset().left + this.renderedPosition("x") + parseFloat( $("#cytoscapeweb").css("border-left-width") ),
-					y: $("#cytoscapeweb").offset().top + this.renderedPosition("y") + this.renderedDimensions("height")/2
-				});
+				displayElementData(this);
 			}
-		});
-		
-		cy.edges().live("click", function(e){
-			if( e.metaKey ){
-				displayElementData(this, {
-					x: e.clientX,
-					y: e.clientY
-				});
-			}
-		});
-		
-		cy.bind("zoom", function(){
-			$(".ui-tooltip").hide();
 		});
 		
 	});

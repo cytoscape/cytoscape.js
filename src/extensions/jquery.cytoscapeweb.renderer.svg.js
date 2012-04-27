607d7631c4d2039f1e04fbe75bb0cdf80675bc68
Max Franz <maxkfranz@gmail.com>
2012-04-27 17:33:20 -0400

    don't use group

--- src/extensions/jquery.cytoscapeweb.renderer.svg.js
+++ src/extensions/jquery.cytoscapeweb.renderer.svg.js
@@ -915,7 +915,7 @@
 		}
 
 		elements.nodes().each(function(){
-			var bb = this.renscratch("svgGroup").getBBox();
+			var bb = this.renscratch("svg").getBBox();
 			var bbLabel = this.renscratch("svgLabel").getBBox();
 
 			update(bb);

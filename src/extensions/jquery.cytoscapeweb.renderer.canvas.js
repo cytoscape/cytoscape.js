9d12a8e367728bdaba08e1a719f8527b952598ed
Max Franz <maxkfranz@gmail.com>
2012-05-02 14:34:27 -0400

    template for canvas renderer

---
+++ src/extensions/jquery.cytoscapeweb.renderer.canvas.js
@@ -0,0 +1,50 @@
+(function($, $$){
+
+	// TODO put default options here
+	var defaults = {
+
+	};
+
+	function CanvasRenderer( options ){
+		this.options = $.extend(true, {}, defaults, options);
+	}
+
+	CanvasRenderer.prototype.notify = function(params){
+		
+	};
+	
+	CanvasRenderer.prototype.zoom = function(params){
+		
+	};
+	
+	CanvasRenderer.prototype.fit = function(params){
+		
+	};
+	
+	CanvasRenderer.prototype.pan = function(params){
+		
+	};
+	
+	CanvasRenderer.prototype.panBy = function(params){
+		
+	};
+	
+	CanvasRenderer.prototype.showElements = function(element){
+		
+	};
+	
+	CanvasRenderer.prototype.hideElements = function(element){
+		
+	};
+	
+	CanvasRenderer.prototype.elementIsVisible = function(element){
+		
+	};
+	
+	CanvasRenderer.prototype.renderedDimensions = function(){
+		
+	};
+
+	$$("renderer", "canvas", CanvasRenderer);
+
+})( jQuery, jQuery.cytoscapeweb );

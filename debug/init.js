16bd0234911196fa61ba86e2f9dd523294dcd7bd
Onur Sumer <s.onur.sumer@gmail.com>
2013-03-12 18:23:10 -0400

    added some tests for the second instance of the debug page

--- debug/init.js
+++ debug/init.js
@@ -221,17 +221,36 @@ $(function(){
 //		$container2.cy(options);
 
 
+		// compound graph in the second instance
 		$container2.cy({
-			elements: {
-				nodes: [ { data: { id: '0' } }, { data: { id: '1' } } ],
-				edges: [ { data: { id: '2', source: '0', target: '1' } } ]
-			},
-			style: defaultSty,
-			
-			ready: function(){
-				window.cy2 = this;
-			}
-		});
+				elements: {
+				   nodes: [ { data: { id: 'n1' } },
+				       { data: { id: 'n2' } },
+				       { data: { id: 'n3', shape: 'rectangle' } },
+				       { data: { id: 'n4', parent: 'n1' } },
+				       { data: { id: 'n5', parent: 'n1', shape: 'triangle' } },
+				       { data: { id: 'node6', parent: 'n2' } },
+				       { data: { id: 'n7', parent: 'n2', shape: 'square' } },
+				       { data: { id: 'n8', parent: 'n4' } },
+				       { data: { id: 'n9', parent: 'n4' } } ],
+				   edges: [ { data: { id: 'e1', source: 'n1', target: 'n3' } },
+				       { data: { id: 'e2', source: 'n3', target: 'n7' } },
+				       { data: { id: 'e3', source: 'node6', target: 'n7' } },
+				       { data: { id: 'e4', source: 'node6', target: 'n9' } },
+				       { data: { id: 'e5', source: 'n8', target: 'n9' } },
+				       { data: { id: 'e6', source: 'n5', target: 'n8' } },
+				       { data: { id: 'e7', source: 'n2', target: 'n4' } }]
+				},
+				style: defaultSty,
+
+				ready: function(){
+				   window.cy2 = this;
+				   cy2.on("click", "node", function(evt){
+				       var node = this;
+				       console.log("%o", node);
+				   });
+				}
+			});
 
 		
 		$("#remove-elements-button").click(function(){
@@ -248,6 +267,50 @@ $(function(){
 		
 		$("#remove-selected-button").click(function(){
 			cy.elements(":selected").remove();
+		});
+
+		$("#compound-remove-selected-button").click(function(){
+			cy2.elements(":selected").remove();
+		});
+
+		$("#compound-hide-selected-button").click(function(){
+			cy2.elements(":selected").hide();
+		});
+
+		$("#compound-show-all-button").click(function(){
+			cy2.elements().show();
+		});
+
+		var numChildren = 0;
+
+		$("#add-child-button").click(function(){
+
+			var parentId = $("#parent-node").val();
+			var nodes = [];
+
+			nodes.push({group: "nodes",
+				           data: {id: "c" + numChildren, parent: parentId},
+				           position: {x: Math.random() * width, y: Math.random() * height}});
+
+			numChildren++;
+
+			cy2.add(nodes);
+		});
+
+		$("#set-random-style").click(function(){
+
+			var nodes = cy2.elements("node:selected");
+
+			for (var i=0; i < nodes.size(); i++)
+			{
+				var shapes = ["triangle", "rectangle", "ellipse", "pentagon"];
+
+				// pick a random shape and dimensions
+				nodes[i].css({"width": Math.round(Math.random() * 50 + 1),
+					"height": Math.round(Math.random() * 50 + 1),
+					"shape": shapes[Math.floor(Math.random() * 4)]});
+			}
+
 		});
 	});
 	

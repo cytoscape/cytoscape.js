e846c754e7561dc4e8d992922a7ec3a8b6e610be
Max <maxkfranz@gmail.com>
2013-05-27 17:45:28 -0400

    update to 2.0.1

--- documentation/js/cytoscape.js
+++ documentation/js/cytoscape.js
@@ -2,7 +2,7 @@
 /* cytoscape.js */
 
 /**
- * This file is part of cytoscape.js 2.0.0-github-snapshot-2013.05.27-17.36.06.
+ * This file is part of cytoscape.js 2.0.1.
  * 
  * Cytoscape.js is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the Free

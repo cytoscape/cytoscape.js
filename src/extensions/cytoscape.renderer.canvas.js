25eadfa200ffdd5483319a6d5a7306fd30a472ee
bumbu <bmbalex@gmail.com>
2013-09-10 23:14:03 +0300

    fix #340 : Scroll to zoom in IE9 and 10

--- src/extensions/cytoscape.renderer.canvas.js
+++ src/extensions/cytoscape.renderer.canvas.js
@@ -855,7 +855,7 @@
 			if (r.zoomData.freeToZoom) {
 				e.preventDefault();
 				
-				var diff = e.wheelDeltaY / 1000 || e.detail / -32;
+				var diff = e.wheelDeltaY / 1000 || e.wheelDelta / 1000 || e.detail / -32;
 				
 				if( cy.panningEnabled() && cy.zoomingEnabled() ){
 					cy.zoom({level: cy.zoom() * Math.pow(10, diff), position: {x: unpos[0], y: unpos[1]}});

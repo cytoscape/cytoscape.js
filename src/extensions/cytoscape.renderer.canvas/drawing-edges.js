a9f5813095ae5c00f536d9733d4d19ba9f987e22
Max Franz <maxkfranz@gmail.com>
2013-11-20 17:15:10 -0500

    more separation of drawing

---
+++ src/extensions/cytoscape.renderer.canvas/drawing-edges.js
@@ -0,0 +1,408 @@
+;(function($$){
+
+	var CanvasRenderer = $$('renderer', 'canvas');
+
+// Draw edge
+	CanvasRenderer.prototype.drawEdge = function(context, edge, drawOverlayInstead) {
+
+		if( !edge.visible() ){
+			return;
+		}
+
+		if( this.hideEdgesOnViewport && (this.dragData.didDrag || this.pinching || this.hoverData.dragging || this.data.wheel || this.swipePanning) ){ return; } // save cycles on pinching
+
+		var startNode, endNode;
+
+		startNode = edge.source()[0];
+		endNode = edge.target()[0];
+		
+		if ( 
+			   edge._private.style["visibility"].value != "visible"
+			|| edge._private.style["display"].value != "element"
+			|| startNode._private.style["visibility"].value != "visible"
+			|| startNode._private.style["display"].value != "element"
+			|| endNode._private.style["visibility"].value != "visible"
+			|| endNode._private.style["display"].value != "element"
+		){
+			return;
+		}
+		
+		var overlayPadding = edge._private.style["overlay-padding"].value;
+		var overlayOpacity = edge._private.style["overlay-opacity"].value;
+		var overlayColor = edge._private.style["overlay-color"].value;
+
+		// Edge color & opacity
+		if( drawOverlayInstead ){
+			context.strokeStyle = "rgba( " + overlayColor[0] + ", " + overlayColor[1] + ", " + overlayColor[2] + ", " + overlayOpacity + " )";
+			context.lineCap = "round";
+
+			if( edge._private.rscratch.edgeType == "self"){
+				context.lineCap = "butt";
+			}
+
+		} else {
+			context.strokeStyle = "rgba(" 
+				+ edge._private.style["line-color"].value[0] + ","
+				+ edge._private.style["line-color"].value[1] + ","
+				+ edge._private.style["line-color"].value[2] + ","
+				+ edge._private.style.opacity.value + ")";
+		}
+
+		// Edge line width
+		if (edge._private.style["width"].value <= 0) {
+			return;
+		}
+		
+		var edgeWidth = edge._private.style["width"].value + (drawOverlayInstead ? 2 * overlayPadding : 0);
+		var lineStyle = drawOverlayInstead ? "solid" : edge._private.style["line-style"].value;
+		context.lineWidth = edgeWidth;
+		
+		this.findEndpoints(edge);
+		
+		if (edge._private.rscratch.edgeType == "self") {
+					
+			var details = edge._private.rscratch;
+			this.drawStyledEdge(edge, context, [details.startX, details.startY, details.cp2ax,
+				details.cp2ay, details.selfEdgeMidX, details.selfEdgeMidY],
+				lineStyle,
+				edgeWidth);
+			
+			this.drawStyledEdge(edge, context, [details.selfEdgeMidX, details.selfEdgeMidY,
+				details.cp2cx, details.cp2cy, details.endX, details.endY],
+				lineStyle,
+				edgeWidth);
+			
+		} else if (edge._private.rscratch.edgeType == "straight") {
+			
+			var nodeDirectionX = endNode._private.position.x - startNode._private.position.x;
+			var nodeDirectionY = endNode._private.position.y - startNode._private.position.y;
+			
+			var edgeDirectionX = edge._private.rscratch.endX - edge._private.rscratch.startX;
+			var edgeDirectionY = edge._private.rscratch.endY - edge._private.rscratch.startY;
+			
+			if (nodeDirectionX * edgeDirectionX
+				+ nodeDirectionY * edgeDirectionY < 0) {
+				
+				edge._private.rscratch.straightEdgeTooShort = true;	
+			} else {
+				
+				var details = edge._private.rscratch;
+				this.drawStyledEdge(edge, context, [details.startX, details.startY,
+				                              details.endX, details.endY],
+				                              lineStyle,
+				                              edgeWidth);
+				
+				edge._private.rscratch.straightEdgeTooShort = false;	
+			}	
+		} else {
+			
+			var details = edge._private.rscratch;
+			this.drawStyledEdge(edge, context, [details.startX, details.startY,
+				details.cp2x, details.cp2y, details.endX, details.endY],
+				lineStyle,
+				edgeWidth);
+			
+		}
+		
+		if (edge._private.rscratch.noArrowPlacement !== true
+				&& edge._private.rscratch.startX !== undefined) {
+			this.drawArrowheads(context, edge, drawOverlayInstead);
+		}
+
+	}
+	
+	var _genPoints = function(pt, spacing, even) {
+		
+		var approxLen = Math.sqrt(Math.pow(pt[4] - pt[0], 2) + Math.pow(pt[5] - pt[1], 2));
+		approxLen += Math.sqrt(Math.pow((pt[4] + pt[0]) / 2 - pt[2], 2) + Math.pow((pt[5] + pt[1]) / 2 - pt[3], 2));
+
+		var pts = Math.ceil(approxLen / spacing); var inc = approxLen / spacing;
+		var pz;
+		
+		if (pts > 0) {
+			pz = new Array(pts * 2);
+		} else {
+			return null;
+		}
+		
+		for (var i = 0; i < pts; i++) {
+			var cur = i / pts;
+			pz[i * 2] = pt[0] * (1 - cur) * (1 - cur) + 2 * (pt[2]) * (1 - cur) * cur + pt[4] * (cur) * (cur);
+			pz[i * 2 + 1] = pt[1] * (1 - cur) * (1 - cur) + 2 * (pt[3]) * (1 - cur) * cur + pt[5] * (cur) * (cur);
+		}
+		
+		return pz;
+	}
+	
+	var _genStraightLinePoints = function(pt, spacing, even) {
+		
+		var approxLen = Math.sqrt(Math.pow(pt[2] - pt[0], 2) + Math.pow(pt[3] - pt[1], 2));
+		
+		var pts = Math.ceil(approxLen / spacing);
+		var pz;
+		
+		if (pts > 0) {
+			pz = new Array(pts * 2);
+		} else {
+			return null;
+		}
+		
+		var lineOffset = [pt[2] - pt[0], pt[3] - pt[1]];
+		for (var i = 0; i < pts; i++) {
+			var cur = i / pts;
+			pz[i * 2] = lineOffset[0] * cur + pt[0];
+			pz[i * 2 + 1] = lineOffset[1] * cur + pt[1];
+		}
+		
+		return pz;
+	}
+	
+	var _genEvenOddpts = function(pt, evenspac, oddspac) {
+		
+		pt1 = _genpts(pt, evenspac);
+		pt2 = _genpts(pt, oddspac);
+	}
+	
+	
+	CanvasRenderer.prototype.drawStyledEdge = function(
+			edge, context, pts, type, width) {
+		
+		// 3 points given -> assume Bezier
+		// 2 -> assume straight
+		
+		var cy = this.data.cy;
+		var zoom = cy.zoom();
+		
+		// Adjusted edge width for dotted
+//		width = Math.max(width * 1.6, 3.4) * zoom;
+
+		//		console.log("w", width);
+		
+		// from http://en.wikipedia.org/wiki/Bézier_curve#Quadratic_curves
+		function qbezierAt(p0, p1, p2, t){
+			return (1 - t)*(1 - t)*p0 + 2*(1 - t)*t*p1 + t*t*p2;
+		}
+
+		if( edge._private.rstyle.bezierPts === undefined ){
+			edge._private.rstyle.bezierPts = [];
+		}
+
+		var nBpts = edge._private.rstyle.bezierPts.length;
+		if( edge.isLoop() ){
+			if( nBpts >= 12 ){
+				edge._private.rstyle.bezierPts = [];
+			} else {
+				// append to current array
+			}
+		} else {
+			edge._private.rstyle.bezierPts = [];
+		}
+
+		var bpts = edge._private.rstyle.bezierPts;
+
+		if( pts.length === 6 ){
+			bpts.push({
+				x: qbezierAt( pts[0], pts[2], pts[4], 0.05 ),
+				y: qbezierAt( pts[1], pts[3], pts[5], 0.05 )
+			});
+
+			bpts.push({
+				x: qbezierAt( pts[0], pts[2], pts[4], 0.25 ),
+				y: qbezierAt( pts[1], pts[3], pts[5], 0.25 )
+			});
+
+			bpts.push({
+				x: qbezierAt( pts[0], pts[2], pts[4], 0.35 ),
+				y: qbezierAt( pts[1], pts[3], pts[5], 0.35 )
+			});
+
+			bpts.push({
+				x: qbezierAt( pts[0], pts[2], pts[4], 0.65 ),
+				y: qbezierAt( pts[1], pts[3], pts[5], 0.65 )
+			});
+
+			bpts.push({
+				x: qbezierAt( pts[0], pts[2], pts[4], 0.75 ),
+				y: qbezierAt( pts[1], pts[3], pts[5], 0.75 )
+			});
+
+			bpts.push({
+				x: qbezierAt( pts[0], pts[2], pts[4], 0.95 ),
+				y: qbezierAt( pts[1], pts[3], pts[5], 0.95 )
+			});
+		}
+
+		if (type == "solid") {
+			
+			context.beginPath();
+			context.moveTo(pts[0], pts[1]);
+			if (pts.length == 3 * 2) {
+				context.quadraticCurveTo(pts[2], pts[3], pts[4], pts[5]);
+			} else {
+				context.lineTo(pts[2], pts[3]);
+			}
+//			context.closePath();
+			context.stroke();
+			
+		} else if (type == "dotted") {
+			
+			var pt;
+			if (pts.length == 3 * 2) {
+				pt = _genPoints(pts, 16, true);
+			} else {
+				pt = _genStraightLinePoints(pts, 16, true);
+			}
+			
+			if (!pt) { return; }
+			
+			var dotRadius = Math.max(width * 1.6, 3.4) * zoom;
+			var bufW = dotRadius * 2, bufH = dotRadius * 2;
+			bufW = Math.max(bufW, 1);
+			bufH = Math.max(bufH, 1);
+			
+			var buffer = this.createBuffer(bufW, bufH);
+			
+			var context2 = buffer[1];
+//			console.log(buffer);
+//			console.log(bufW, bufH);
+			
+			// Draw on buffer
+			context2.setTransform(1, 0, 0, 1, 0, 0);
+			context2.clearRect(0, 0, bufW, bufH);
+			
+			context2.fillStyle = context.strokeStyle;
+			context2.beginPath();
+			context2.arc(bufW/2, bufH/2, dotRadius * 0.5, 0, Math.PI * 2, false);
+			context2.fill();
+			
+			// Now use buffer
+			context.beginPath();
+			//context.save();
+			
+			for (var i=0; i<pt.length/2; i++) {
+				
+//				context.beginPath();
+//				context.arc(pt[i*2], pt[i*2+1], width * 0.5, 0, Math.PI * 2, false);
+//				context.fill();
+				
+				context.drawImage(
+						buffer[0],
+						pt[i*2] - bufW/2 / zoom,
+						pt[i*2+1] - bufH/2 / zoom,
+						bufW / zoom,
+						bufH / zoom);
+			}
+			
+			//context.restore();
+			
+		} else if (type == "dashed") {
+			var pt;
+			if (pts.length == 3 * 2) {
+				pt = _genPoints(pts, 14, true);
+			} else {
+				pt = _genStraightLinePoints(pts, 14, true);
+			}
+			if (!pt) { return; }
+			
+//			var dashSize = Math.max(width * 1.6, 3.4);
+//			dashSize = Math.min(dashSize)
+			
+			//var bufW = width * 2 * zoom, bufH = width * 2.5 * zoom;
+			var bufW = width * 2 * zoom
+			var bufH = 7.8 * zoom;
+			bufW = Math.max(bufW, 1);
+			bufH = Math.max(bufH, 1);
+			
+			var buffer = this.createBuffer(bufW, bufH);
+			var context2 = buffer[1];
+
+			// Draw on buffer
+			context2.setTransform(1, 0, 0, 1, 0, 0);
+			context2.clearRect(0, 0, bufW, bufH);
+			
+			if (context.strokeStyle) {
+				context2.strokeStyle = context.strokeStyle;
+			}
+			
+			context2.lineWidth = width * cy.zoom();
+			
+	//		context2.fillStyle = context.strokeStyle;
+			
+			context2.beginPath();
+			context2.moveTo(bufW / 2, bufH * 0.2);
+			context2.lineTo(bufW / 2,  bufH * 0.8);
+			
+	//		context2.arc(bufH, dotRadius, dotRadius * 0.5, 0, Math.PI * 2, false);
+			
+	//		context2.fill();
+			context2.stroke();
+			
+			//context.save();
+			
+			// document.body.appendChild(buffer[0]);
+			
+			var quadraticBezierVaryingTangent = false;
+			var rotateVector, angle;
+			
+			// Straight line; constant tangent angle
+			if (pts.length == 2 * 2) {
+				rotateVector = [pts[2] - pts[0], pts[3] - pt[1]];
+				
+				angle = Math.acos((rotateVector[0] * 0 + rotateVector[1] * -1) / Math.sqrt(rotateVector[0] * rotateVector[0] 
+						+ rotateVector[1] * rotateVector[1]));
+	
+				if (rotateVector[0] < 0) {
+					angle = -angle + 2 * Math.PI;
+				}
+			} else if (pts.length == 3 * 2) {
+				quadraticBezierVaryingTangent = true;
+			}
+			
+			for (var i=0; i<pt.length/2; i++) {
+				
+				var p = i / (Math.max(pt.length/2 - 1, 1));
+			
+				// Quadratic bezier; varying tangent
+				// So, use derivative of quadratic Bezier function to find tangents
+				if (quadraticBezierVaryingTangent) {
+					rotateVector = [2 * (1-p) * (pts[2] - pts[0]) 
+					                	+ 2 * p * (pts[4] - pts[2]),
+					                    2 * (1-p) * (pts[3] - pts[1]) 
+					                    + 2 * p * (pts[5] - pts[3])];
+	
+					angle = Math.acos((rotateVector[0] * 0 + rotateVector[1] * -1) / Math.sqrt(rotateVector[0] * rotateVector[0] 
+								+ rotateVector[1] * rotateVector[1]));
+	
+					if (rotateVector[0] < 0) {
+						angle = -angle + 2 * Math.PI;
+					}
+				}
+				
+				context.translate(pt[i*2], pt[i*2+1]);
+				
+				context.rotate(angle);
+				context.translate(-bufW/2 / zoom, -bufH/2 / zoom);
+				
+				context.drawImage(
+						buffer[0],
+						0,
+						0,
+						bufW / zoom,
+						bufH / zoom);
+				
+				context.translate(bufW/2 / zoom, bufH/2 / zoom);
+				context.rotate(-angle);
+				
+				context.translate(-pt[i*2], -pt[i*2+1]);
+				
+			}
+			
+			
+			//context.restore();
+		} else {
+			this.drawStyledEdge(edge, context, pts, "solid", width);
+		}
+		
+	};
+
+})( cytoscape );
---
+++ src/extensions/cytoscape.renderer.canvas/drawing-label-text.js
@@ -0,0 +1,190 @@
+;(function($$){
+
+	var CanvasRenderer = $$('renderer', 'canvas');
+
+	// Draw edge text
+	CanvasRenderer.prototype.drawEdgeText = function(context, edge) {
+	
+		if( !edge.visible() ){
+			return;
+		}
+
+		if( this.hideEdgesOnViewport && (this.dragData.didDrag || this.pinching || this.hoverData.dragging || this.data.wheel || this.swipePanning) ){ return; } // save cycles on pinching
+
+		var computedSize = edge._private.style["font-size"].pxValue * edge.cy().zoom();
+		var minSize = edge._private.style["min-zoomed-font-size"].pxValue;
+
+		if( computedSize < minSize ){
+			return;
+		}
+	
+		// Calculate text draw position
+		
+		context.textAlign = "center";
+		context.textBaseline = "middle";
+		
+		var textX, textY;	
+		var edgeCenterX, edgeCenterY;
+		
+		if (edge._private.rscratch.edgeType == "self") {
+			edgeCenterX = edge._private.rscratch.selfEdgeMidX;
+			edgeCenterY = edge._private.rscratch.selfEdgeMidY;
+		} else if (edge._private.rscratch.edgeType == "straight") {
+			edgeCenterX = (edge._private.rscratch.startX
+				+ edge._private.rscratch.endX) / 2;
+			edgeCenterY = (edge._private.rscratch.startY
+				+ edge._private.rscratch.endY) / 2;
+		} else if (edge._private.rscratch.edgeType == "bezier") {
+			edgeCenterX = 0.25 * edge._private.rscratch.startX
+				+ 2 * 0.5 * 0.5 * edge._private.rscratch.cp2x
+				+ (0.5 * 0.5) * edge._private.rscratch.endX;
+			edgeCenterY = Math.pow(1 - 0.5, 2) * edge._private.rscratch.startY
+				+ 2 * (1 - 0.5) * 0.5 * edge._private.rscratch.cp2y
+				+ (0.5 * 0.5) * edge._private.rscratch.endY;
+		}
+		
+		textX = edgeCenterX;
+		textY = edgeCenterY;
+
+		// add center point to style so bounding box calculations can use it
+		var rstyle = edge._private.rstyle;
+		rstyle.labelX = textX;
+		rstyle.labelY = textY;
+		
+		this.drawText(context, edge, textX, textY);
+	};
+
+	// Draw node text
+	CanvasRenderer.prototype.drawNodeText = function(context, node) {
+		
+		if ( !node.visible() ) {
+			return;
+		}
+
+		var computedSize = node._private.style["font-size"].pxValue * node.cy().zoom();
+		var minSize = node._private.style["min-zoomed-font-size"].pxValue;
+
+		if( computedSize < minSize ){
+			return;
+		}
+	
+		var textX, textY;
+
+		//var nodeWidth = node._private.style["width"].value;
+		//var nodeHeight = node._private.style["height"].value;
+		var nodeWidth = this.getNodeWidth(node);
+		var nodeHeight = this.getNodeHeight(node);
+	
+		// Find text position
+		var textHalign = node._private.style["text-halign"].strValue;
+		if (textHalign == "left") {
+			// Align right boundary of text with left boundary of node
+			context.textAlign = "right";
+			textX = node._private.position.x - nodeWidth / 2;
+		} else if (textHalign == "right") {
+			// Align left boundary of text with right boundary of node
+			context.textAlign = "left";
+			textX = node._private.position.x + nodeWidth / 2;
+		} else if (textHalign == "center") {
+			context.textAlign = "center";
+			textX = node._private.position.x;
+		} else {
+			// Same as center
+			context.textAlign = "center";
+			textX = node._private.position.x;
+		}
+		
+		var textValign = node._private.style["text-valign"].strValue;
+		if (textValign == "top") {
+			context.textBaseline = "bottom";
+			textY = node._private.position.y - nodeHeight / 2;
+		} else if (textValign == "bottom") {
+			context.textBaseline = "top";
+			textY = node._private.position.y + nodeHeight / 2;
+		} else if (textValign == "middle" || textValign == "center") {
+			context.textBaseline = "middle";
+			textY = node._private.position.y;
+		} else {
+			// same as center
+			context.textBaseline = "middle";
+			textY = node._private.position.y;
+		}
+		
+		this.drawText(context, node, textX, textY);
+	};
+	
+	// Draw text
+	CanvasRenderer.prototype.drawText = function(context, element, textX, textY) {
+	
+		var parentOpacity = 1;
+		var parents = element.parents();
+		for( var i = 0; i < parents.length; i++ ){
+			var parent = parents[i];
+			var opacity = parent._private.style.opacity.value;
+
+			parentOpacity = opacity * parentOpacity;
+
+			if( opacity === 0 ){
+				return;
+			}
+		}
+
+		// Font style
+		var labelStyle = element._private.style["font-style"].strValue;
+		var labelSize = element._private.style["font-size"].value + "px";
+		var labelFamily = element._private.style["font-family"].strValue;
+		var labelVariant = element._private.style["font-variant"].strValue;
+		var labelWeight = element._private.style["font-weight"].strValue;
+		
+		context.font = labelStyle + " " + labelWeight + " "
+			+ labelSize + " " + labelFamily;
+		
+		var text = String(element._private.style["content"].value);
+		var textTransform = element._private.style["text-transform"].value;
+		
+		if (textTransform == "none") {
+		} else if (textTransform == "uppercase") {
+			text = text.toUpperCase();
+		} else if (textTransform == "lowercase") {
+			text = text.toLowerCase();
+		}
+		
+		// Calculate text draw position based on text alignment
+		
+		// so text outlines aren't jagged
+		context.lineJoin = 'round';
+
+		context.fillStyle = "rgba(" 
+			+ element._private.style["color"].value[0] + ","
+			+ element._private.style["color"].value[1] + ","
+			+ element._private.style["color"].value[2] + ","
+			+ (element._private.style["text-opacity"].value
+			* element._private.style["opacity"].value * parentOpacity) + ")";
+		
+		context.strokeStyle = "rgba(" 
+			+ element._private.style["text-outline-color"].value[0] + ","
+			+ element._private.style["text-outline-color"].value[1] + ","
+			+ element._private.style["text-outline-color"].value[2] + ","
+			+ (element._private.style["text-opacity"].value
+			* element._private.style["opacity"].value * parentOpacity) + ")";
+		
+		if (text != undefined) {
+			var lineWidth = 2  * element._private.style["text-outline-width"].value; // *2 b/c the stroke is drawn centred on the middle
+			if (lineWidth > 0) {
+				context.lineWidth = lineWidth;
+				context.strokeText(text, textX, textY);
+			}
+
+			// Thanks sysord@github for the isNaN checks!
+			if (isNaN(textX)) { textX = 0; }
+			if (isNaN(textY)) { textY = 0; }
+
+			context.fillText("" + text, textX, textY);
+
+			// record the text's width for use in bounding box calc
+			element._private.rstyle.labelWidth = context.measureText( text ).width;
+		}
+	};
+
+	
+})( cytoscape );

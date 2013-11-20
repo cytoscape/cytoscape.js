5e0638bfa9b1861c59bc78855c6eecd27e118618
Max Franz <maxkfranz@gmail.com>
2013-11-20 13:50:06 -0500

    separate element caching

--- src/extensions/cytoscape.renderer.canvas/todo.js
+++ src/extensions/cytoscape.renderer.canvas/todo.js
@@ -1638,56 +1638,6 @@
 	CanvasRenderer.prototype.init = function() { };
 	}
 	
-	// @O Caching functions
-	{
-	CanvasRenderer.prototype.getCachedNodes = function() {
-		var data = this.data; var cy = this.data.cy;
-		
-		if (data.cache == undefined) {
-			data.cache = {};
-		}
-		
-		if (data.cache.cachedNodes == undefined) {
-			data.cache.cachedNodes = cy.nodes();
-		}
-		
-		return data.cache.cachedNodes;
-	}
-	
-	CanvasRenderer.prototype.updateNodesCache = function() {
-		var data = this.data; var cy = this.data.cy;
-		
-		if (data.cache == undefined) {
-			data.cache = {};
-		}
-		
-		data.cache.cachedNodes = cy.nodes();
-	}
-	
-	CanvasRenderer.prototype.getCachedEdges = function() {
-		var data = this.data; var cy = this.data.cy;
-		
-		if (data.cache == undefined) {
-			data.cache = {};
-		}
-		
-		if (data.cache.cachedEdges == undefined) {
-			data.cache.cachedEdges = cy.edges();
-		}
-		
-		return data.cache.cachedEdges;
-	}
-	
-	CanvasRenderer.prototype.updateEdgesCache = function() {
-		var data = this.data; var cy = this.data.cy;
-		
-		if (data.cache == undefined) {
-			data.cache = {};
-		}
-		
-		data.cache.cachedEdges = cy.edges();
-	}
-	}
 	
 	// @O High-level collision application functions
 

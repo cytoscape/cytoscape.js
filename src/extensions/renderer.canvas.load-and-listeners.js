05cff4625f7e1acf744d585b41f859ec354f37e3
Max <maxkfranz@gmail.com>
2014-01-09 16:45:16 -0500

    remove deprecated e.returnValue

--- src/extensions/renderer.canvas.load-and-listeners.js
+++ src/extensions/renderer.canvas.load-and-listeners.js
@@ -441,7 +441,6 @@
 				if(e.stopPropagation) e.stopPropagation();
     			if(e.preventDefault) e.preventDefault();
    				e.cancelBubble=true;
-    			e.returnValue=false;
     			return false;
     		}
 		}, false);

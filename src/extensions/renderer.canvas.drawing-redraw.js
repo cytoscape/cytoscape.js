f407da7278d952dd0597464331514aba576d0c83
Max Franz <maxkfranz@gmail.com>
2014-01-17 12:50:50 -0500

    add 0 timeout to make touch devices more responsive

--- src/extensions/renderer.canvas.drawing-redraw.js
+++ src/extensions/renderer.canvas.drawing-redraw.js
@@ -146,259 +146,265 @@
 
 		// } console.timeEnd('init')
 
-	
+		function drawToContext(){
 
-		if (data.canvasNeedsRedraw[CanvasRenderer.DRAG] || data.canvasNeedsRedraw[CanvasRenderer.NODE] || drawAllLayers) {
-			//NB : VERY EXPENSIVE
-			//console.time('edgectlpts'); for( var looper = 0; looper <= looperMax; looper++ ){
+			if (data.canvasNeedsRedraw[CanvasRenderer.DRAG] || data.canvasNeedsRedraw[CanvasRenderer.NODE] || drawAllLayers) {
+				//NB : VERY EXPENSIVE
+				//console.time('edgectlpts'); for( var looper = 0; looper <= looperMax; looper++ ){
 
-			if( r.hideEdgesOnViewport && (r.pinching || r.hoverData.dragging || r.data.wheel || r.swipePanning) ){ 
-			} else {
-				r.findEdgeControlPoints(edges);
-			}
-
-			//} console.timeEnd('edgectlpts')
-
-		
-
-			// console.time('sort'); for( var looper = 0; looper <= looperMax; looper++ ){
-			var elements = r.getCachedZSortedEles();
-			// } console.timeEnd('sort')
-
-			// console.time('updatecompounds'); for( var looper = 0; looper <= looperMax; looper++ ){
-			// no need to update graph if there is no compound node
-			if ( cy.hasCompoundNodes() )
-			{
-				r.updateAllCompounds(elements);
-			}
-			// } console.timeEnd('updatecompounds')
-		}
-		
-		var elesInDragLayer;
-		var elesNotInDragLayer;
-		var element;
-
-
-		// console.time('drawing'); for( var looper = 0; looper <= looperMax; looper++ ){
-		if (data.canvasNeedsRedraw[CanvasRenderer.NODE] || drawAllLayers) {
-			// console.log("redrawing node layer");
-		  
-		  	if( !elesInDragLayer || !elesNotInDragLayer ){
-				elesInDragLayer = [];
-				elesNotInDragLayer = [];
-
-				for (var index = 0; index < elements.length; index++) {
-					element = elements[index];
-
-					if ( element._private.rscratch.inDragLayer ) {
-						elesInDragLayer.push( element );
-					} else {
-						elesNotInDragLayer.push( element );
-					}
-				}
-			}	
-
-			var context = forcedContext || data.canvases[CanvasRenderer.NODE].getContext("2d");
-
-			context.setTransform(1, 0, 0, 1, 0, 0);
-			!forcedContext && context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-			
-			if( !drawAllLayers ){
-				context.translate(effectivePan.x, effectivePan.y);
-				context.scale(effectiveZoom, effectiveZoom);
-			}
-			if( forcedPan ){
-				context.translate(forcedPan.x, forcedPan.y);
-			} 
-			if( forcedZoom ){
-				context.scale(forcedZoom, forcedZoom);
-			}
-			
-			for (var index = 0; index < elesNotInDragLayer.length; index++) {
-				element = elesNotInDragLayer[index];
-				
-				if (element._private.group == "nodes") {
-					r.drawNode(context, element);
-					
-				} else if (element._private.group == "edges") {
-					r.drawEdge(context, element);
-				}
-			}
-			
-			for (var index = 0; index < elesNotInDragLayer.length; index++) {
-				element = elesNotInDragLayer[index];
-				
-				if (element._private.group == "nodes") {
-					r.drawNodeText(context, element);
-				} else if (element._private.group == "edges") {
-					r.drawEdgeText(context, element);
+				if( r.hideEdgesOnViewport && (r.pinching || r.hoverData.dragging || r.data.wheel || r.swipePanning) ){ 
+				} else {
+					r.findEdgeControlPoints(edges);
 				}
 
-				// draw the overlay
-				if (element._private.group == "nodes") {
-					r.drawNode(context, element, true);
-				} else if (element._private.group == "edges") {
-					r.drawEdge(context, element, true);
+				//} console.timeEnd('edgectlpts')
+
+			
+
+				// console.time('sort'); for( var looper = 0; looper <= looperMax; looper++ ){
+				var elements = r.getCachedZSortedEles();
+				// } console.timeEnd('sort')
+
+				// console.time('updatecompounds'); for( var looper = 0; looper <= looperMax; looper++ ){
+				// no need to update graph if there is no compound node
+				if ( cy.hasCompoundNodes() )
+				{
+					r.updateAllCompounds(elements);
 				}
+				// } console.timeEnd('updatecompounds')
 			}
 			
-			if( !drawAllLayers ){
-				data.canvasNeedsRedraw[CanvasRenderer.NODE] = false; 
-			}
-		}
-		
-		if (data.canvasNeedsRedraw[CanvasRenderer.DRAG] || drawAllLayers) {
-		  
-			if( !elesInDragLayer || !elesNotInDragLayer ){
-				elesInDragLayer = [];
-				elesNotInDragLayer = [];
-
-				for (var index = 0; index < elements.length; index++) {
-					element = elements[index];
-
-					if ( element._private.rscratch.inDragLayer ) {
-						elesInDragLayer.push( element );
-					} else {
-						elesNotInDragLayer.push( element );
-					}
-				}
-			}
-
-			var context = forcedContext || data.canvases[CanvasRenderer.DRAG].getContext("2d");
-			
-			if( !drawAllLayers ){
-				context.setTransform(1, 0, 0, 1, 0, 0);
-				!forcedContext && context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-				
-				context.translate(effectivePan.x, effectivePan.y);
-				context.scale(effectiveZoom, effectiveZoom);
-			} 
-			if( forcedPan ){
-				context.translate(forcedPan.x, forcedPan.y);
-			} 
-			if( forcedZoom ){
-				context.scale(forcedZoom, forcedZoom);
-			}
-			
+			var elesInDragLayer;
+			var elesNotInDragLayer;
 			var element;
 
-			for (var index = 0; index < elesInDragLayer.length; index++) {
-				element = elesInDragLayer[index];
-				
-				if (element._private.group == "nodes") {
-					r.drawNode(context, element);
-				} else if (element._private.group == "edges") {
-					r.drawEdge(context, element);
-				}
-			}
-			
-			for (var index = 0; index < elesInDragLayer.length; index++) {
-				element = elesInDragLayer[index];
-				
-				if (element._private.group == "nodes") {
-					r.drawNodeText(context, element);
-				} else if (element._private.group == "edges") {
-					r.drawEdgeText(context, element);
-				}
 
-				// draw the overlay
-				if (element._private.group == "nodes") {
-					r.drawNode(context, element, true);
-				} else if (element._private.group == "edges") {
-					r.drawEdge(context, element, true);
-				}
-			}
-			
-			if( !drawAllLayers ){
-				data.canvasNeedsRedraw[CanvasRenderer.DRAG] = false;
-			}
-		}
-		
-		if (data.canvasNeedsRedraw[CanvasRenderer.SELECT_BOX]) {
-			// console.log("redrawing selection box");
-		  
-			var context = forcedContext || data.canvases[CanvasRenderer.SELECT_BOX].getContext("2d");
-			
-			if( !drawAllLayers ){
+			// console.time('drawing'); for( var looper = 0; looper <= looperMax; looper++ ){
+			if (data.canvasNeedsRedraw[CanvasRenderer.NODE] || drawAllLayers) {
+				// console.log("redrawing node layer");
+			  
+			  	if( !elesInDragLayer || !elesNotInDragLayer ){
+					elesInDragLayer = [];
+					elesNotInDragLayer = [];
+
+					for (var index = 0; index < elements.length; index++) {
+						element = elements[index];
+
+						if ( element._private.rscratch.inDragLayer ) {
+							elesInDragLayer.push( element );
+						} else {
+							elesNotInDragLayer.push( element );
+						}
+					}
+				}	
+
+				var context = forcedContext || data.canvases[CanvasRenderer.NODE].getContext("2d");
+
 				context.setTransform(1, 0, 0, 1, 0, 0);
 				!forcedContext && context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-			
-				context.translate(effectivePan.x, effectivePan.y);
-				context.scale(effectiveZoom, effectiveZoom);		
-			} 
-			if( forcedPan ){
-				context.translate(forcedPan.x, forcedPan.y);
-			} 
-			if( forcedZoom ){
-				context.scale(forcedZoom, forcedZoom);
+				
+				if( !drawAllLayers ){
+					context.translate(effectivePan.x, effectivePan.y);
+					context.scale(effectiveZoom, effectiveZoom);
+				}
+				if( forcedPan ){
+					context.translate(forcedPan.x, forcedPan.y);
+				} 
+				if( forcedZoom ){
+					context.scale(forcedZoom, forcedZoom);
+				}
+				
+				for (var index = 0; index < elesNotInDragLayer.length; index++) {
+					element = elesNotInDragLayer[index];
+					
+					if (element._private.group == "nodes") {
+						r.drawNode(context, element);
+						
+					} else if (element._private.group == "edges") {
+						r.drawEdge(context, element);
+					}
+				}
+				
+				for (var index = 0; index < elesNotInDragLayer.length; index++) {
+					element = elesNotInDragLayer[index];
+					
+					if (element._private.group == "nodes") {
+						r.drawNodeText(context, element);
+					} else if (element._private.group == "edges") {
+						r.drawEdgeText(context, element);
+					}
+
+					// draw the overlay
+					if (element._private.group == "nodes") {
+						r.drawNode(context, element, true);
+					} else if (element._private.group == "edges") {
+						r.drawEdge(context, element, true);
+					}
+				}
+				
+				if( !drawAllLayers ){
+					data.canvasNeedsRedraw[CanvasRenderer.NODE] = false; 
+				}
 			}
 			
-			var coreStyle = cy.style()._private.coreStyle;
+			if (data.canvasNeedsRedraw[CanvasRenderer.DRAG] || drawAllLayers) {
+			  
+				if( !elesInDragLayer || !elesNotInDragLayer ){
+					elesInDragLayer = [];
+					elesNotInDragLayer = [];
 
-			if (data.select[4] == 1) {
-				var zoom = data.cy.zoom();
-				var borderWidth = coreStyle["selection-box-border-width"].value / zoom;
+					for (var index = 0; index < elements.length; index++) {
+						element = elements[index];
+
+						if ( element._private.rscratch.inDragLayer ) {
+							elesInDragLayer.push( element );
+						} else {
+							elesNotInDragLayer.push( element );
+						}
+					}
+				}
+
+				var context = forcedContext || data.canvases[CanvasRenderer.DRAG].getContext("2d");
 				
-				context.lineWidth = borderWidth;
-				context.fillStyle = "rgba(" 
-					+ coreStyle["selection-box-color"].value[0] + ","
-					+ coreStyle["selection-box-color"].value[1] + ","
-					+ coreStyle["selection-box-color"].value[2] + ","
-					+ coreStyle["selection-box-opacity"].value + ")";
+				if( !drawAllLayers ){
+					context.setTransform(1, 0, 0, 1, 0, 0);
+					!forcedContext && context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+					
+					context.translate(effectivePan.x, effectivePan.y);
+					context.scale(effectiveZoom, effectiveZoom);
+				} 
+				if( forcedPan ){
+					context.translate(forcedPan.x, forcedPan.y);
+				} 
+				if( forcedZoom ){
+					context.scale(forcedZoom, forcedZoom);
+				}
 				
-				context.fillRect(
-					data.select[0],
-					data.select[1],
-					data.select[2] - data.select[0],
-					data.select[3] - data.select[1]);
+				var element;
+
+				for (var index = 0; index < elesInDragLayer.length; index++) {
+					element = elesInDragLayer[index];
+					
+					if (element._private.group == "nodes") {
+						r.drawNode(context, element);
+					} else if (element._private.group == "edges") {
+						r.drawEdge(context, element);
+					}
+				}
 				
-				if (borderWidth > 0) {
-					context.strokeStyle = "rgba(" 
-						+ coreStyle["selection-box-border-color"].value[0] + ","
-						+ coreStyle["selection-box-border-color"].value[1] + ","
-						+ coreStyle["selection-box-border-color"].value[2] + ","
+				for (var index = 0; index < elesInDragLayer.length; index++) {
+					element = elesInDragLayer[index];
+					
+					if (element._private.group == "nodes") {
+						r.drawNodeText(context, element);
+					} else if (element._private.group == "edges") {
+						r.drawEdgeText(context, element);
+					}
+
+					// draw the overlay
+					if (element._private.group == "nodes") {
+						r.drawNode(context, element, true);
+					} else if (element._private.group == "edges") {
+						r.drawEdge(context, element, true);
+					}
+				}
+				
+				if( !drawAllLayers ){
+					data.canvasNeedsRedraw[CanvasRenderer.DRAG] = false;
+				}
+			}
+			
+			if (data.canvasNeedsRedraw[CanvasRenderer.SELECT_BOX]) {
+				// console.log("redrawing selection box");
+			  
+				var context = forcedContext || data.canvases[CanvasRenderer.SELECT_BOX].getContext("2d");
+				
+				if( !drawAllLayers ){
+					context.setTransform(1, 0, 0, 1, 0, 0);
+					!forcedContext && context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+				
+					context.translate(effectivePan.x, effectivePan.y);
+					context.scale(effectiveZoom, effectiveZoom);		
+				} 
+				if( forcedPan ){
+					context.translate(forcedPan.x, forcedPan.y);
+				} 
+				if( forcedZoom ){
+					context.scale(forcedZoom, forcedZoom);
+				}
+				
+				var coreStyle = cy.style()._private.coreStyle;
+
+				if (data.select[4] == 1) {
+					var zoom = data.cy.zoom();
+					var borderWidth = coreStyle["selection-box-border-width"].value / zoom;
+					
+					context.lineWidth = borderWidth;
+					context.fillStyle = "rgba(" 
+						+ coreStyle["selection-box-color"].value[0] + ","
+						+ coreStyle["selection-box-color"].value[1] + ","
+						+ coreStyle["selection-box-color"].value[2] + ","
 						+ coreStyle["selection-box-opacity"].value + ")";
 					
-					context.strokeRect(
+					context.fillRect(
 						data.select[0],
 						data.select[1],
 						data.select[2] - data.select[0],
 						data.select[3] - data.select[1]);
+					
+					if (borderWidth > 0) {
+						context.strokeStyle = "rgba(" 
+							+ coreStyle["selection-box-border-color"].value[0] + ","
+							+ coreStyle["selection-box-border-color"].value[1] + ","
+							+ coreStyle["selection-box-border-color"].value[2] + ","
+							+ coreStyle["selection-box-opacity"].value + ")";
+						
+						context.strokeRect(
+							data.select[0],
+							data.select[1],
+							data.select[2] - data.select[0],
+							data.select[3] - data.select[1]);
+					}
+				}
+
+				if( data.bgActivePosistion ){
+					var zoom = data.cy.zoom();
+					var pos = data.bgActivePosistion;
+
+					context.fillStyle = "rgba(" 
+						+ coreStyle["active-bg-color"].value[0] + ","
+						+ coreStyle["active-bg-color"].value[1] + ","
+						+ coreStyle["active-bg-color"].value[2] + ","
+						+ coreStyle["active-bg-opacity"].value + ")";
+
+					context.beginPath();
+					context.arc(pos.x, pos.y, coreStyle["active-bg-size"].pxValue / zoom, 0, 2 * Math.PI); 
+					context.fill();
+				}
+				
+				if( !drawAllLayers ){
+					data.canvasNeedsRedraw[CanvasRenderer.SELECT_BOX] = false; 
 				}
 			}
 
-			if( data.bgActivePosistion ){
-				var zoom = data.cy.zoom();
-				var pos = data.bgActivePosistion;
+			// } console.timeEnd('drawing')
 
-				context.fillStyle = "rgba(" 
-					+ coreStyle["active-bg-color"].value[0] + ","
-					+ coreStyle["active-bg-color"].value[1] + ","
-					+ coreStyle["active-bg-color"].value[2] + ","
-					+ coreStyle["active-bg-opacity"].value + ")";
+			var endTime = +new Date;
 
-				context.beginPath();
-				context.arc(pos.x, pos.y, coreStyle["active-bg-size"].pxValue / zoom, 0, 2 * Math.PI); 
-				context.fill();
+			if( r.averageRedrawTime === undefined ){
+				r.averageRedrawTime = endTime - startTime;
 			}
-			
-			if( !drawAllLayers ){
-				data.canvasNeedsRedraw[CanvasRenderer.SELECT_BOX] = false; 
-			}
+
+			// use a weighted average with a bias from the previous average so we don't spike so easily
+			r.averageRedrawTime = r.averageRedrawTime/2 + (endTime - startTime)/2;
+			//console.log('actual: %i, average: %i', endTime - startTime, this.averageRedrawTime);
 		}
 
-		// } console.timeEnd('drawing')
-
-		var endTime = +new Date;
-
-		if( r.averageRedrawTime === undefined ){
-			r.averageRedrawTime = endTime - startTime;
+		if( !forcedContext ){
+			setTimeout(drawToContext, 0); // makes direct renders to screen a bit more responsive
+		} else {
+			drawToContext();
 		}
-
-		// use a weighted average with a bias from the previous average so we don't spike so easily
-		r.averageRedrawTime = r.averageRedrawTime/2 + (endTime - startTime)/2;
-		//console.log('actual: %i, average: %i', endTime - startTime, this.averageRedrawTime);
-
 
 		if( !forcedContext && !r.initrender ){
 			r.initrender = true;

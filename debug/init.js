17608d8df49557daa861f0baf3ea4defebc086a0
Max <maxkfranz@gmail.com>
2014-01-23 15:08:32 -0500

    separate out debug instance 2 from compound node guys

--- debug/init.js
+++ debug/init.js
@@ -219,41 +219,6 @@ $(function(){
 			});
 		});
 
-
-//		$container2.cy(options);
-
-		// compound graph in the second instance
-		$container2.cy({
-				elements: {
-				   nodes: [{ data: { id: 'n8', parent: 'n4' } },
-					   { data: { id: 'n9', parent: 'n4' } },
-					   { data: { id: 'n4', parent: 'n1' } },
-					   { data: { id: 'n5', parent: 'n1', shape: 'triangle' } },
-					   { data: { id: 'n1' } },
-				       { data: { id: 'n2' } },
-				       { data: { id: 'node6', parent: 'n2' } },
-				       { data: { id: 'n7', parent: 'n2', shape: 'square' } },
-					   { data: { id: 'n3', parent: 'non-auto', shape: 'rectangle' } },
-					   { data: { id: 'non-auto'}}],
-				   edges: [ { data: { id: 'e1', source: 'n1', target: 'n3' } },
-				       { data: { id: 'e2', source: 'n3', target: 'n7' } },
-				       { data: { id: 'e3', source: 'node6', target: 'n7' } },
-				       { data: { id: 'e4', source: 'node6', target: 'n9' } },
-				       { data: { id: 'e5', source: 'n8', target: 'n9' } },
-				       { data: { id: 'e6', source: 'n5', target: 'n8' } },
-				       { data: { id: 'e7', source: 'n2', target: 'n4' } }]
-				},
-				style: defaultSty,
-
-				ready: function(){
-				   window.cy2 = this;
-				   cy2.on("click", "node", function(evt){
-				       var node = this;
-				       console.log("%o", node);
-				   });
-				}
-			});
-
 		
 		$("#remove-elements-button").click(function(){
 			var n = number("nodes");
@@ -271,6 +236,39 @@ $(function(){
 			cy.elements(":selected").remove();
 		});
 
+	});
+
+	// compound graph in the second instance
+	$container2.cy({
+		elements: {
+		   nodes: [{ data: { id: 'n8', parent: 'n4' } },
+			   { data: { id: 'n9', parent: 'n4' } },
+			   { data: { id: 'n4', parent: 'n1' } },
+			   { data: { id: 'n5', parent: 'n1', shape: 'triangle' } },
+			   { data: { id: 'n1' } },
+		       { data: { id: 'n2' } },
+		       { data: { id: 'node6', parent: 'n2' } },
+		       { data: { id: 'n7', parent: 'n2', shape: 'square' } },
+			   { data: { id: 'n3', parent: 'non-auto', shape: 'rectangle' } },
+			   { data: { id: 'non-auto'}}],
+		   edges: [ { data: { id: 'e1', source: 'n1', target: 'n3' } },
+		       { data: { id: 'e2', source: 'n3', target: 'n7' } },
+		       { data: { id: 'e3', source: 'node6', target: 'n7' } },
+		       { data: { id: 'e4', source: 'node6', target: 'n9' } },
+		       { data: { id: 'e5', source: 'n8', target: 'n9' } },
+		       { data: { id: 'e6', source: 'n5', target: 'n8' } },
+		       { data: { id: 'e7', source: 'n2', target: 'n4' } }]
+		},
+		style: defaultSty,
+
+		ready: function(){
+		   window.cy2 = this;
+		   cy2.on("click", "node", function(evt){
+		       var node = this;
+		       console.log("%o", node);
+		   });
+		}
+	}).cy(function(){
 		$("#compound-remove-selected-button").click(function(){
 			cy2.elements(":selected").remove();
 		});

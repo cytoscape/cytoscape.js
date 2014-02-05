42627048c155acdf2fbf8b1577d086372424ece6
Max Franz <maxkfranz@gmail.com>
2014-02-05 16:00:32 -0500

    cytoscape.js pulled into mocha tests

--- test/example.js
+++ test/example.js
@@ -1,9 +1,52 @@
-var assert = require("assert")
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    })
-  })
-})
+var expect = require('chai').expect;
+var cytoscape = require('../build/cytoscape', cytoscape);
+
+describe('Initialisation', function(){
+
+  it('A node with the same ID as an earlier one is not added', function(done){
+    cytoscape({
+      renderer: {
+        name: 'null'
+      },
+      layout: {
+        name: 'null'
+      },
+      elements: {
+        nodes: [
+          { data: { id: 'n1', foo: 'one' } },
+          { data: { id: 'n2', foo: 'two' } },
+          { data: { id: 'n1', foo: 'what is this guy doing here' } }
+        ]
+      }, 
+      ready: function(){
+        var cy = this;
+
+        expect( cy.elements().size() ).to.equal(2);
+        expect( cy.$('#n1').data('foo') ).to.equal('one');
+        
+        done();
+      }
+    });
+  });
+
+  it('An edge with bad source and target does not get created', function(done){
+    cytoscape({
+      renderer: {
+        name: "null"
+      },
+      layout: {
+        name: "null"
+      },
+      elements: {
+        edges: [ { data: { source: "n1", target: "n2" } } ]
+      },
+      ready: function(){
+        ok( false, "Didn't get exception" );
+        
+        start();
+      }
+    });
+  });
+
+
+});

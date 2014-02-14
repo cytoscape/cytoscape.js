b7d9649c5450490a1f503ac879d3e90b53ba9408
Rich Trott <rtrott@gmail.com>
2014-02-14 11:57:28 -0800

    use !== to conform with coding practice in all other inequality comparisons in the file

--- src/core.js
+++ src/core.js
@@ -161,7 +161,7 @@
 		},
 
 		registered: function(){
-			if( this._private && this._private.instanceId != null ){
+			if( this._private && this._private.instanceId !== null ){
 				return true;
 			} else {
 				return false;

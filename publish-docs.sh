e50e17cc1f7f4008de543c3acdb14441b9ec3482
Max Franz <maxkfranz@gmail.com>
2014-03-12 17:35:00 -0400

    fix scripts from makefile

--- publish-docs.sh
+++ publish-docs.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-CP = cp -R
-RM = rm -rf
-GIT = git
-TEMP_DIR = /tmp
-DOC_DIR = documentation
+CP=cp -R
+RM=rm -rf
+GIT=git
+TEMP_DIR=/tmp
+DOC_DIR=documentation
 
-$(RM) $(TEMP_DIR)/cytoscape.js
-$(GIT) clone -b gh-pages https://github.com/cytoscape/cytoscape.js.git $(TEMP_DIR)/cytoscape.js
-$(CP) $(DOC_DIR)/* $(TEMP_DIR)/cytoscape.js
-$(CD) $(TEMP_DIR)/cytoscape.js
-$(GIT) add -A
-$(GIT) commit -a -m "updating docs to $(VERSION)"
-$(GIT) push origin
+$RM $TEMP_DIR/cytoscape.js
+$GIT clone -b gh-pages https://github.com/cytoscape/cytoscape.js.git $TEMP_DIR/cytoscape.js
+$CP $DOC_DIR/* $TEMP_DIR/cytoscape.js
+$CD $TEMP_DIR/cytoscape.js
+$GIT add -A
+$GIT commit -a -m "updating docs to $VERSION"
+$GIT push origin

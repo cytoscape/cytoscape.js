084ef10137b412b0ad8308136f63db6f40e07602
Max Franz <maxkfranz@gmail.com>
2014-08-19 16:58:08 -0400

    prototype generalised cola adaptor; initial layout integration
    TODO more complicated cola params
    TODO push cola adaptor back to cola codebase when done

--- src/extensions/layout.cola.js
+++ src/extensions/layout.cola.js
@@ -2,6 +2,22 @@
 
   // default layout options
   var defaults = {
+    animate: true,
+    maxSimulationTime: 4000, // max length in ms to run the layout
+    fit: true, // on every layout reposition of nodes, fit the viewport
+    padding: 30, // padding around the simulation
+
+    // layout forces & constraints
+    avoidOverlaps: true, // if true, prevents overlap of node bounding boxes
+    edgeLength: function(edge){ return 100; }, // edge length in simulation
+    edgeSymLength: function(edge){ return 5; }, // symmetric diff edge length in simulation
+
+    // iterations of cola algorithm; uses default values on undefined
+    unconstrIter: undefined, // unconstrained initial layout iterations
+    userConstIter: undefined, // initial layout iterations with user-specified constraints
+    allConstIter: undefined, // initial layout iterations with all constraints including non-overlap
+
+    // layout event callbacks
     ready: function(){}, // on layoutready
     stop: function(){} // on layoutstop
   };
@@ -14,34 +30,131 @@
 
   // runs the layout
   ColaLayout.prototype.run = function(){
+    var layout = this;
     var options = this.options;
     var cy = options.cy; // cy is automatically populated for us in the constructor
-    cy.trigger('layoutstart');
+    var eles = cy.elements();
+    var nodes = eles.nodes();
+    var edges = eles.edges();
+    var ready = false;
 
-    // puts all nodes at (0, 0)
-    cy.nodes().positions(function(){
-      return {
-        x: 0,
-        y: 0
-      };
+    var updateNodePositions = function(){
+      nodes.positions(function(i, node){
+        var pos = node._private.position;
+        var scratch = node._private.scratch.cola;
+
+        pos.x = scratch.x;
+        pos.y = scratch.y;
+      });
+
+      if( !ready ){
+        onReady();
+        ready = true;
+      }
+
+      if( options.fit ){
+        cy.fit( options.padding );
+      }
+    };
+
+    var onDone = function(){
+      // trigger layoutstop when the layout stops (e.g. finishes)
+      cy.one('layoutstop', options.stop);
+      cy.trigger({ type: 'layoutstop', layout: layout });
+    };
+
+    var onReady = function(){
+      // trigger layoutready when each node has had its position set at least once
+      cy.one('layoutready', options.ready);
+      cy.trigger({ type: 'layoutready', layout: layout });
+    };
+
+    var adaptor = cola.adaptor({
+      trigger: function( e ){ // on sim event
+        switch( e.type ){
+          case 'tick':
+            if( options.animate ){
+              updateNodePositions();
+            }
+            break;
+
+          case 'end': 
+            updateNodePositions();
+            onDone();
+            break;
+        }
+      },
+
+      kick: function( tick ){ // kick off the simulation
+        if( options.animate ){
+          var frame = function(){
+            if( tick() ){ return; }
+
+            $$.util.requestAnimationFrame( frame );
+          };
+
+          $$.util.requestAnimationFrame( frame );
+        } else {
+          while( !tick() ){}
+        }
+      },
+
+      on: function( type, listener ){}, // dummy; not needed
+
+      drag: function(){} // TODO
     });
+    layout.adaptor = adaptor;
 
-    // trigger layoutready when each node has had its position set at least once
-    cy.one('layoutready', options.ready);
-    cy.trigger('layoutready');
+    var nodeIdToIndex = {};
+    adaptor.nodes( nodes.map(function( node, i ){
+      nodeIdToIndex[ node.id() ] = i;
 
-    // trigger layoutstop when the layout stops (e.g. finishes)
-    cy.one('layoutstop', options.stop);
-    cy.trigger('layoutstop');
+      return node._private.scratch.cola = {
+        width: node.outerWidth(),
+        height: node.outerHeight()
+      };
+    }) );
+
+    adaptor.links( edges.map(function( edge, i ){
+      return edge._private.scratch.cola = {
+        source: nodeIdToIndex[ edge.data('source') ],
+        target: nodeIdToIndex[ edge.data('target') ],
+        distance: $$.is.number(options.edgeLength) ? options.edgeLength : options.edgeLength.apply( edge, [edge] ),
+        symDistance: $$.is.number(options.edgeSymLength) ? options.edgeSymLength : options.edgeSymLength.apply( edge, [edge] ),
+      };
+    }) );
+
+    adaptor
+      .size([ cy.width(), cy.height() ])
+      .linkDistance(function( link ){
+        return link.distance;
+      })
+      // TODO
+      // .symmetricDiffLinkLengths(function( link ){
+      //   return link.symDistance;
+      // })
+      .avoidOverlaps( options.avoidOverlaps )
+      .handleDisconnected( true )
+      //.jaccardLinkLengths() // TODO
+      //.flowLayout() // TODO
+      //.constraints() // TODO
+      //.distanceMatrix() // TODO
+      //.groups() // TODO
+      //.powerGraphGroups() // TODO
+      .start( options.unconstrIter, options.userConstIter, options.allConstIter)
+    ;
+
+    cy.trigger({ type: 'layoutstart', layout: layout });
+
+    setTimeout(function(){
+      adaptor.stop();
+    }, options.maxSimulationTime);
+
   };
 
   // called on continuous layouts to stop them before they finish
   ColaLayout.prototype.stop = function(){
-    var options = this.options;
-    var cy = options.cy;
-
-    cy.one('layoutstop', options.stop);
-    cy.trigger('layoutstop');
+    adaptor.stop();
   };
 
   // register the layout

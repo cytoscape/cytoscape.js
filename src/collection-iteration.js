956495d06cb7edd122bb127d447dd82997abf973
Max <maxkfranz@gmail.com>
2014-09-04 15:16:49 -0400

    Edges connected to compounds, draw z-order off #666
    reorganise z sort and collated draw
    
    Conflicts:
    	src/collection-iteration.js
    	src/extensions/renderer.canvas.drawing-redraw.js

--- src/collection-iteration.js
+++ src/collection-iteration.js
@@ -104,76 +104,71 @@
 
     sortByZIndex: function(){
       return this.sort( $$.Collection.zIndexSort );
+    },
+
+    zDepth: function(){
+      var ele = this[0];
+      if( !ele ){ return undefined; }
+
+      var _p = ele._private;
+      var group = _p.group;
+
+      if( group === 'nodes' ){
+        return _p.data.parent ? ele.parents().size() : 0;
+      } else {
+        var src = _p.source;
+        var tgt = _p.target;
+        var srcDepth = src._private.data.parent ? src.parents().size() : 0;
+        var tgtDepth = tgt._private.data.parent ? tgt.parents().size() : 0;
+
+        return Math.max( srcDepth - 1, tgtDepth - 1, 0 ) + 0.5; // depth of deepest parent and just a bit above
+      }
     }
   });
 
-
   $$.Collection.zIndexSort = function(a, b){
-    var cy = a._private.cy;
-    var hasCompoundNodes = cy._private.hasCompoundNodes;
-
-    var elementDepth = function(ele) {
-      if( ele._private.group === 'nodes' ){
-        return ele._private.data.parent ? ele.parents().length : 0;
-        
-      } else if( ele._private.group === 'edges' ){
-        var source = ele._private.source;
-        var target = ele._private.target;
-
-        var sourceDepth = source._private.data.parent ? source.parents().length : 0;
-        var targetDepth = target._private.data.parent ? target.parents().length : 0;
-
-        return Math.max( sourceDepth, targetDepth );
-
-      } else {
-        return 0;
-      }
-    };
-
-    var result = a._private.style['z-index'].value - b._private.style['z-index'].value;
-
+    var cy = a.cy();
+    var a_p = a._private;
+    var b_p = b._private;
+    var zDiff = a_p.style['z-index'].value - b_p.style['z-index'].value;
     var depthA = 0;
     var depthB = 0;
+    var hasCompoundNodes = cy.hasCompoundNodes();
+    var aIsNode = a_p.group === 'nodes';
+    var aIsEdge = a_p.group === 'edges';
+    var bIsNode = b_p.group === 'nodes';
+    var bIsEdge = b_p.group === 'edges';
 
     // no need to calculate element depth if there is no compound node
     if( hasCompoundNodes ){
-      depthA = elementDepth(a);
-      depthB = elementDepth(b);
+      depthA = a.zDepth();
+      depthB = b.zDepth();
     }
 
-    // if both elements has same depth,
-    // then edges should be drawn first
-    if( depthA - depthB === 0 ){
-      // 'a' is a node, it should be drawn later
-      if( a._private.group === 'nodes'
-        && b._private.group === 'edges' ){
-        return 1;
-      }
-      
-      // 'a' is an edge, it should be drawn first
-      else if( a._private.group === 'edges'
-        && b._private.group === 'nodes' ){
-        return -1;
-      }
+    var depthDiff = depthA - depthB;
+    var sameDepth = depthDiff === 0;
 
-      // both nodes or both edges
-      else {
-        if( result === 0 ){ // same z-index => compare indices in the core (order added to graph w/ last on top)
-          return a._private.index - b._private.index;
+    if( sameDepth ){
+      
+      if( aIsNode && bIsEdge ){
+        return 1; // 'a' is a node, it should be drawn later       
+      
+      } else if( aIsEdge && bIsNode ){
+        return -1; // 'a' is an edge, it should be drawn first
+
+      } else { // both nodes or both edges
+        if( zDiff === 0 ){ // same z-index => compare indices in the core (order added to graph w/ last on top)
+          return a_p.index - b_p.index;
         } else {
-          return result;
+          return zDiff;
         }
       }
-    }
-
+    
     // elements on different level
-    else {
-      // deeper element should be drawn later
-      return depthA - depthB;
+    } else {
+      return depthDiff; // deeper element should be drawn later
     }
 
-    // return zero if z-index values are not the same
-    return 0;
   };
   
 })( cytoscape );

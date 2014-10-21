956cf2910f0bd2f49bba418142ff7eba011b3f43
Max <maxkfranz@gmail.com>
2014-10-21 12:46:44 -0400

    improve builds list

--- documentation/download/dlmaker.js
+++ documentation/download/dlmaker.js
@@ -2,10 +2,39 @@ var fs = require('fs');
 var Handlebars = require('handlebars');
 var encoding = 'utf8';
 
-module.exports = function( next ){
-  var files = fs.readdirSync('.').sort().reverse().filter(function( file ){
-    return file.match(/\.zip$/);
+var vercmp = function(a, b){
+  var aNums = getVerNums(a);
+  var bNums = getVerNums(b);
+
+  for( var i = 0; i < aNums.length; i++ ){
+    if( aNums[i] === bNums[i] ){ continue; }
+
+    return aNums[i] - bNums[i];
+  }
+
+  return 0;
+};
+
+var getVerNums = function( filename ){
+  var format = /cytoscape\.js-(\d+\.\d+\.\d+)\.zip/;
+
+  return filename.match( format )[1].split('.').map(function( digit ){
+    return parseInt( digit, 10 );
   });
+}
+
+var filenameHasVer = function( file ){
+  try{
+    getVerNums( file );
+  } catch(e){
+    return false;
+  }
+
+  return true;
+};
+
+module.exports = function( next ){
+  var files = fs.readdirSync('.').filter( filenameHasVer ).sort( vercmp ).reverse();
 
   var builds = files.map(function( file ){
     return {
@@ -22,5 +51,7 @@ module.exports = function( next ){
 
   fs.writeFileSync('index.html', html, encoding);
 
-  next && next();
-};
+  next();
+}
+
+

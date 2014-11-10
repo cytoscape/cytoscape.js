4aa34057938730cad34eec4f475b8e9e7aa31b4e
Max Franz <maxkfranz@gmail.com>
2014-11-10 13:10:03 -0500

    ele.move() should conveniently handle compound edge cases #706

--- test/collection-graph-manipulation.js
+++ test/collection-graph-manipulation.js
@@ -133,6 +133,43 @@ describe('Collection graph manipulation', function(){
       expect( cy.$('#child').parent().length ).to.equal(0);
     });
 
+    it('should move descendants and connected edges', function(){
+      cy = cytoscape({
+        elements: {
+          nodes: [
+            { data: { id: 'a', parent: 'b' } },
+            { data: { id: 'b' } },
+            { data: { id: 'c'} },
+            { data: { id: 'd', parent: 'c' } },
+            { data: { id: 'e' } }
+          ], 
+          
+          edges: [
+            { data: { id: 'ae', weight: 1, source: 'a', target: 'e' } },
+            { data: { id: 'ab', weight: 3, source: 'a', target: 'b' } },
+            { data: { id: 'be', weight: 4, source: 'b', target: 'e' } },
+            { data: { id: 'bc', weight: 5, source: 'b', target: 'c' } },
+            { data: { id: 'ce', weight: 6, source: 'c', target: 'e' } },
+            { data: { id: 'cd', weight: 2, source: 'c', target: 'd' } },
+            { data: { id: 'de', weight: 7, source: 'd', target: 'e' } }
+          ]
+        }
+      });
+
+      var N = cy.elements().length;
+      var Nn = cy.nodes().length;
+      var Ne = cy.edges().length;
+
+      cy.$('#c').move({ parent: 'b' });
+
+      expect( cy.elements().length ).to.equal( N );
+      expect( cy.edges().length ).to.equal( Ne );
+      expect( cy.nodes().length ).to.equal( Nn );
+      expect( cy.$('#c').parent().id() ).to.equal( 'b' );
+      expect( cy.$('#de').source().same( cy.$('#d') ) ).to.be.true;
+      expect( cy.$('#de').target().same( cy.$('#e') ) ).to.be.true;
+    });
+
   });
 
 

4906ee4401dbd56f5387651dab9abc52f57831b2
Max <maxkfranz@gmail.com>
2015-03-13 16:03:26 -0400

    spread layout cleanup and fixes
    re #814

--- src/extensions/layout.spread.js
+++ src/extensions/layout.spread.js
@@ -28,20 +28,7 @@
   function SpreadLayout(options){
     this.options = $$.util.extend({}, defaults, options);
   }
-  
-  function scriptPath() {
-    var scripts = document.getElementsByTagName('SCRIPT');
-    var path = '';
-    if(scripts && scripts.length>0) {
-        for(var i in scripts) {
-            if(scripts[i].src && scripts[i].src.match(/script\.js$/)) {
-                path = scripts[i].src.replace(/(.*)script\.js$/, '$1');
-            }
-        }
-    }
-    return path;
-  }
-  
+   
   function cellCentroid( cell ) {
     var hes = cell.halfedges;
     var area = 0, x = 0, y = 0;
@@ -78,9 +65,9 @@
     var options = this.options;
     var cy = options.cy;
     var allNodes = cy.nodes();
-    var nodes = cy.elements("node:visible");
+    var nodes = cy.nodes();
     //var allEdges = cy.edges();
-    var edges = cy.elements("edge:visible");
+    var edges = cy.edges();
     var container = cy.container();
     var cWidth = container.clientWidth;
     var cHeight = container.clientHeight;
@@ -106,13 +93,10 @@
     var width  = simBB.x2 - simBB.x1;
     var height = simBB.y2 - simBB.y1;
 
-    layout.one("layoutready", options.ready);
-    layout.trigger("layoutready");
-
     // Get start time
-    var startTime = new Date();
+    var startTime = Date.now();
 
-    // arbor doesn't work with just 1 node
+    // layout doesn't work with just 1 node
     if( nodes.size() <= 1 ){
       if( options.fit ){
         cy.reset();
@@ -124,8 +108,11 @@
       });
 
       // Get end time
-      var endTime = new Date();
+      var endTime = Date.now();
       console.info("Layout on "+nodes.size()+" nodes took " + (endTime - startTime) + " ms");
+
+      layout.one("layoutready", options.ready);
+      layout.trigger("layoutready");
 
       layout.one("layoutstop", options.stop);
       layout.trigger("layoutstop");
@@ -159,7 +146,7 @@
         pData['edges'].push({src:srcNodeId,tgt:tgtNodeId});
       });
 
-//Decleration
+   //Decleration
    var t1 = $$.Thread();
     // And to add the required scripts
     //EXTERNAL 1
@@ -172,212 +159,211 @@
     t1.require(cellCentroid);
     
     
-  $$.Promise.all([ // Untill all threads are completed 
-  t1.pass(pData).run(function(pData){ // WORKER 1      
-        // I need to retrieve the important data
-        var lWidth    = pData['width'];
-        var lHeight   = pData['height']; 
-        var lMinDist  = pData['minDist'];
-        var lExpFact  = pData['expFact'];
-        var lMaxExpIt = pData['maxExpIt'];
+    $$.Promise.all([ t1.pass(pData).run(function(pData){     
+      // I need to retrieve the important data
+      var lWidth    = pData['width'];
+      var lHeight   = pData['height']; 
+      var lMinDist  = pData['minDist'];
+      var lExpFact  = pData['expFact'];
+      var lMaxExpIt = pData['maxExpIt'];
 
-        /*
-         * FIRST STEP: Application of the Fruchterman-Reingold algorithm
-         * 
-         * We use the version implemented by the foograph library
-         *  
-         * Ref.: https://code.google.com/p/foograph/
-         */
-        
-        // We need to create an instance of a graph compatible with the library
-        var frg = new foograph.Graph("FRgraph", false);
-        
-        var frgNodes = {};
-        
-        // Then we have to add the vertices
-        var dataVertices = pData['vertices'];
-        for( var ni = 0; ni < dataVertices.length; ++ni ) {
-          var id = dataVertices[ni]['id'];
-          var v = new foograph.Vertex( id, Math.round(Math.random()*lHeight), Math.round(Math.random()*lHeight) );
-          frgNodes[id] = v;
-          frg.insertVertex(v);
-        }
-        
-        var dataEdges = pData['edges'];
-        for( var ei = 0; ei < dataEdges.length; ++ei ) {
-          var srcNodeId = dataEdges[ei]['src'];
-          var tgtNodeId = dataEdges[ei]['tgt'];
-          frg.insertEdge( "", 1, frgNodes[srcNodeId], frgNodes[tgtNodeId] );
-        }
+      /*
+       * FIRST STEP: Application of the Fruchterman-Reingold algorithm
+       * 
+       * We use the version implemented by the foograph library
+       *  
+       * Ref.: https://code.google.com/p/foograph/
+       */
+      
+      // We need to create an instance of a graph compatible with the library
+      var frg = new foograph.Graph("FRgraph", false);
+      
+      var frgNodes = {};
+      
+      // Then we have to add the vertices
+      var dataVertices = pData['vertices'];
+      for( var ni = 0; ni < dataVertices.length; ++ni ) {
+        var id = dataVertices[ni]['id'];
+        var v = new foograph.Vertex( id, Math.round(Math.random()*lHeight), Math.round(Math.random()*lHeight) );
+        frgNodes[id] = v;
+        frg.insertVertex(v);
+      }
+      
+      var dataEdges = pData['edges'];
+      for( var ei = 0; ei < dataEdges.length; ++ei ) {
+        var srcNodeId = dataEdges[ei]['src'];
+        var tgtNodeId = dataEdges[ei]['tgt'];
+        frg.insertEdge( "", 1, frgNodes[srcNodeId], frgNodes[tgtNodeId] );
+      }
 
-        var fv = frg.vertices;
-    
-        // Then we apply the layout
-        var frLayoutManager = new foograph.ForceDirectedVertexLayout(lWidth, lHeight, 400, false, 20);
-        
-        frLayoutManager.layout(frg);
-    
-        /*
-         * SECOND STEP: Tiding up of the graph.
-         * 
-         * We use the method described by Gansner and North, based on Voronoi
-         * diagrams.
-         * 
-         * Ref: doi:10.1007/3-540-37623-2_28
-         */
-    
-        // We calculate the Voronoi diagram dor the position of the nodes
-        var voronoi = new Voronoi();
-        var bbox = { xl:0, xr:lWidth, yt:0, yb:lHeight };
-        var vSites = [];
-        for( var i = 0; i < fv.length; ++i ) {
-          vSites[fv[i].label] = fv[i];
-        }
-        
-        function checkMinDist(ee) {
-          var infractions = 0;
-          // Then we check if the minimum distance is satisfied
-          for( var eei = 0; eei < ee.length; ++eei ) {
-            var e = ee[eei];
-            if( (e.lSite != null) && (e.rSite != null) && sitesDistance(e.lSite,e.rSite) < lMinDist ) {
-              ++infractions;
-            }
+      var fv = frg.vertices;
+
+      // Then we apply the layout
+      var frLayoutManager = new foograph.ForceDirectedVertexLayout(lWidth, lHeight, 400, false, 20);
+      
+      frLayoutManager.layout(frg);
+
+      /*
+       * SECOND STEP: Tiding up of the graph.
+       * 
+       * We use the method described by Gansner and North, based on Voronoi
+       * diagrams.
+       * 
+       * Ref: doi:10.1007/3-540-37623-2_28
+       */
+
+      // We calculate the Voronoi diagram dor the position of the nodes
+      var voronoi = new Voronoi();
+      var bbox = { xl:0, xr:lWidth, yt:0, yb:lHeight };
+      var vSites = [];
+      for( var i = 0; i < fv.length; ++i ) {
+        vSites[fv[i].label] = fv[i];
+      }
+      
+      function checkMinDist(ee) {
+        var infractions = 0;
+        // Then we check if the minimum distance is satisfied
+        for( var eei = 0; eei < ee.length; ++eei ) {
+          var e = ee[eei];
+          if( (e.lSite != null) && (e.rSite != null) && sitesDistance(e.lSite,e.rSite) < lMinDist ) {
+            ++infractions;
           }
-          return infractions;
         }
-        
-        var diagram = voronoi.compute(fv, bbox);
-        
-        // Then we reposition the nodes at the centroid of their Voronoi cells
-        var cells = diagram.cells;
-        for( var i = 0; i < cells.length; ++i ) {
-          var cell = cells[i];
-          var site = cell.site;
-          var centroid = cellCentroid(cell);
-          var currv = vSites[site.label];
-          currv.x = centroid.x;
-          currv.y = centroid.y;
-        }
+        return infractions;
+      }
+      
+      var diagram = voronoi.compute(fv, bbox);
+      
+      // Then we reposition the nodes at the centroid of their Voronoi cells
+      var cells = diagram.cells;
+      for( var i = 0; i < cells.length; ++i ) {
+        var cell = cells[i];
+        var site = cell.site;
+        var centroid = cellCentroid(cell);
+        var currv = vSites[site.label];
+        currv.x = centroid.x;
+        currv.y = centroid.y;
+      }
 
-        if( lExpFact < 0.0 ) {
-          // Calculates the expanding factor
-          lExpFact = Math.max(0.05,Math.min(0.10,lMinDist / Math.sqrt((lWidth*lHeight)/fv.length) * 0.5));
-          //console.info("Expanding factor is " + (options.expandingFactor * 100.0) + "%");
-        }
-        
-        var prevInfractions = checkMinDist(diagram.edges);
-        //console.info("Initial infractions " + prevInfractions);
-    
-        var bStop = (prevInfractions <= 0);
-        
-        var voronoiIteration = 0;
-        var expandIteration  = 0;
-        
-        var initWidth  = lWidth;
-        
-        while( !bStop ) {
-          ++voronoiIteration;
-          for( var it = 0; it <= 4; ++it ) {
-            voronoi.recycle(diagram);
-            diagram = voronoi.compute(fv, bbox);
-            
-            // Then we reposition the nodes at the centroid of their Voronoi cells
-            cells = diagram.cells;
-            for( var i = 0; i < cells.length; ++i ) {
-              var cell = cells[i];
-              var site = cell.site;
-              var centroid = cellCentroid(cell);
-              var currv = vSites[site.label];
-              currv.x = centroid.x;
-              currv.y = centroid.y;
-            }
-          }
+      if( lExpFact < 0.0 ) {
+        // Calculates the expanding factor
+        lExpFact = Math.max(0.05,Math.min(0.10,lMinDist / Math.sqrt((lWidth*lHeight)/fv.length) * 0.5));
+        //console.info("Expanding factor is " + (options.expandingFactor * 100.0) + "%");
+      }
+      
+      var prevInfractions = checkMinDist(diagram.edges);
+      //console.info("Initial infractions " + prevInfractions);
+
+      var bStop = (prevInfractions <= 0);
+      
+      var voronoiIteration = 0;
+      var expandIteration  = 0;
+      
+      var initWidth  = lWidth;
+      
+      while( !bStop ) {
+        ++voronoiIteration;
+        for( var it = 0; it <= 4; ++it ) {
+          voronoi.recycle(diagram);
+          diagram = voronoi.compute(fv, bbox);
           
-          var currInfractions = checkMinDist(diagram.edges);
-          //console.info("Current infractions " + currInfractions);
-    
-          if( currInfractions <= 0 ) {
-            bStop = true;
-          } else {
-            if( currInfractions >= prevInfractions || voronoiIteration >= 4 ) {
-              if( expandIteration >= lMaxExpIt ) {
-                bStop = true;
-              } else {
-                lWidth  += lWidth  * lExpFact;
-                lHeight += lHeight * lExpFact;
-                bbox = { xl:0, xr:lWidth, yt:0, yb:lHeight };
-                ++expandIteration;
-                voronoiIteration = 0;
-                //console.info("Expanded to ("+width+","+height+")");
-              }
+          // Then we reposition the nodes at the centroid of their Voronoi cells
+          cells = diagram.cells;
+          for( var i = 0; i < cells.length; ++i ) {
+            var cell = cells[i];
+            var site = cell.site;
+            var centroid = cellCentroid(cell);
+            var currv = vSites[site.label];
+            currv.x = centroid.x;
+            currv.y = centroid.y;
+          }
+        }
+        
+        var currInfractions = checkMinDist(diagram.edges);
+        //console.info("Current infractions " + currInfractions);
+
+        if( currInfractions <= 0 ) {
+          bStop = true;
+        } else {
+          if( currInfractions >= prevInfractions || voronoiIteration >= 4 ) {
+            if( expandIteration >= lMaxExpIt ) {
+              bStop = true;
+            } else {
+              lWidth  += lWidth  * lExpFact;
+              lHeight += lHeight * lExpFact;
+              bbox = { xl:0, xr:lWidth, yt:0, yb:lHeight };
+              ++expandIteration;
+              voronoiIteration = 0;
+              //console.info("Expanded to ("+width+","+height+")");
             }
           }
-          prevInfractions = currInfractions;
         }
-        
-        // Prepare the data to output
-        pData['width']   = lWidth;
-        pData['height']  = lHeight;
-        pData['expIt']   = expandIteration;
-        pData['expFact'] = lExpFact;
+        prevInfractions = currInfractions;
+      }
+      
+      // Prepare the data to output
+      pData['width']   = lWidth;
+      pData['height']  = lHeight;
+      pData['expIt']   = expandIteration;
+      pData['expFact'] = lExpFact;
 
-        pData['vertices'] = [];
-        for( var i = 0; i < fv.length; ++i ) {
-          pData['vertices'].push({id:fv[i].label,x:fv[i].x,y:fv[i].y});
-        }
-        return pData;
-     
-     })
-]).then(function( thens ){
-  var pData = thens[0];
-  // First we retrieve the important data
-        var expandIteration = pData['expIt'];
-        var dataVertices = pData['vertices'];
-        var vertices = [];
-        for( var i = 0; i < dataVertices.length; ++i ) {
-          var dv = dataVertices[i];
-          vertices[dv.id] = {x:dv.x,y:dv.y};
-        }
-        /*
-         * FINALLY:
-         *
-         * We position the nodes based on the calculation
-         */
-        allNodes.positions(
-          function(i,node) {
-            var pos = node._private.position;
-            pos.x = simBB.x1;
-            pos.y = simBB.y1;
-          });
+      pData['vertices'] = [];
+      for( var i = 0; i < fv.length; ++i ) {
+        pData['vertices'].push({id:fv[i].label,x:fv[i].x,y:fv[i].y});
+      }
+      return pData;
+       
+    }) ]).then(function( thens ){
+      var pData = thens[0];
 
-        nodes.positions(
-          function(i,node) {
-            var id = node._private.data.id;
-            var pos = node._private.position;
-            var vertex = vertices[id];
-            pos.x = Math.round(simBB.x1+vertex.x);
-            pos.y = Math.round(simBB.y1+vertex.y);
-          });
+      // First we retrieve the important data
+      var expandIteration = pData['expIt'];
+      var dataVertices = pData['vertices'];
+      var vertices = [];
+      for( var i = 0; i < dataVertices.length; ++i ) {
+        var dv = dataVertices[i];
+        vertices[dv.id] = {x:dv.x,y:dv.y};
+      }
+      /*
+       * FINALLY:
+       *
+       * We position the nodes based on the calculation
+       */
+      nodes.positions(
+        function(i,node) {
+          var id = node._private.data.id;
+          var pos = node._private.position;
+          var vertex = vertices[id];
 
-        if( options.fit && expandIteration > 0 ){
-          cy.fit(options.padding);
-        } else {
-          cy.reset();
-        }
-        
-        cy.nodes().rtrigger("position");
+          return {
+            x: Math.round(simBB.x1+vertex.x),
+            y: Math.round(simBB.y1+vertex.y)
+          };
+        });
 
-        // Get end time
-        var startTime = pData['startTime'];
-        var endTime = new Date();
-        console.info("Layout on "+dataVertices.length+" nodes took " + (endTime - startTime) + " ms");
-        layout.one("layoutstop", options.stop);
-        layout.trigger("layoutstop");
-t1.stop();
- next();
-});
+      if( options.fit && expandIteration > 0 ){
+        cy.fit(options.padding);
+      } else {
+        cy.reset();
+      }
+      
+      cy.nodes().rtrigger("position");
+
+      layout.one("layoutready", options.ready);
+      layout.trigger("layoutready");
+
+      // Get end time
+      var startTime = pData['startTime'];
+      var endTime = new Date();
+      console.info("Layout on "+dataVertices.length+" nodes took " + (endTime - startTime) + " ms");
+      layout.one("layoutstop", options.stop);
+      layout.trigger("layoutstop");
+
+      t1.stop();
+    });
+
     return this;
-  };
+  }; // run
 
   SpreadLayout.prototype.stop = function(){
   };

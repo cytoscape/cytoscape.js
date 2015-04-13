b314354fd230664420a13d81e0efacc363ace814
Max <maxkfranz@gmail.com>
2015-04-13 16:14:38 -0400

    release prep

--- documentation/js/cytoscape.js
+++ documentation/js/cytoscape.js
@@ -1,5 +1,5 @@
 /*!
- * This file is part of Cytoscape.js 2.3.14.
+ * This file is part of Cytoscape.js 2.3.15.
  * 
  * Cytoscape.js is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the Free
@@ -29,7 +29,7 @@ var cytoscape;
     return cytoscape.init.apply(cytoscape, arguments);
   };
 
-  $$.version = '2.3.14';
+  $$.version = '2.3.15';
   
   // allow functional access to cytoscape.js
   // e.g. var cyto = $.cytoscape({ selector: "#foo", ... });

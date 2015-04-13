bf74684fabc4d0d2389c5368b8cdfb257aa5e780
Max <maxkfranz@gmail.com>
2015-04-13 12:59:27 -0400

    add meteor support #899

---
+++ package.js
@@ -0,0 +1,24 @@
+var packageJson = JSON.parse(Npm.require("fs").readFileSync('package.json'));
+
+Package.describe({
+  name: 'maxkfranz:cytoscape',
+  version: packageJson.version,
+  summary: packageJson.description,
+  git: packageJson.repository.url,
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  //api.versionsFrom('1.0');
+  api.export('cytoscape');
+  api.addFiles([
+    'dist/cytoscape.js',
+    'export.js'
+  ]);
+});
+
+Package.onTest(function(api) {
+  api.use('maxkfranz:cytoscape');
+  api.use('tinytest');
+  api.addFiles('test-meteor.js');
+});

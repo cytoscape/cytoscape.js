14100f4ea85c1d28ed3d6393df7eb1fd5d4e5ee4
maxkfranz <maxkfranz@gmail.com>
2015-04-15 13:01:02 -0400

    cola should animate by default

--- src/extensions/layout.cola.js
+++ src/extensions/layout.cola.js
@@ -2,7 +2,7 @@
 
   // default layout options
   var defaults = {
-    animate: false, // whether to show the layout as it's running
+    animate: true, // whether to show the layout as it's running
     refresh: 1, // number of ticks per frame; higher is faster but more jerky
     maxSimulationTime: 4000, // max length in ms to run the layout
     ungrabifyWhileSimulating: false, // so you can't drag nodes during layout

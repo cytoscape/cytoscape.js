63266a6e7eea0afe4375845900ffb8699a83f025
Max <maxkfranz@gmail.com>
2015-06-23 16:21:35 -0400

    ndexSizeError in CanvasRenderer.prototype.drawInscribedImage #785

--- src/extensions/renderer.canvas.drawing-images.js
+++ src/extensions/renderer.canvas.drawing-images.js
@@ -19,7 +19,22 @@
     
     return image;
   };
+  
+  CRp.safeDrawImage = function( context, img, x, y, w, h ){
+    var r = this;
     
+    try {
+      context.drawImage( img, 0, 0, img.width, img.height, x, y, w, h );
+    } catch(e){
+      r.data.canvasNeedsRedraw[CanvasRenderer.NODE] = true;
+      r.data.canvasNeedsRedraw[CanvasRenderer.DRAG] = true;
+      
+      r.drawingImage = true;
+      
+      r.redraw();
+    }
+  };
+  
   CRp.drawInscribedImage = function(context, img, node) {
     var r = this;
     var nodeX = node._private.position.x;
@@ -117,7 +132,8 @@
         }
       }
 
-      context.drawImage( img, 0, 0, img.width, img.height, x, y, w, h );
+      // context.drawImage( img, 0, 0, img.width, img.height, x, y, w, h );
+      r.safeDrawImage( context, img, x, y, w, h );
 
       if( shouldClip ){
         context.restore();

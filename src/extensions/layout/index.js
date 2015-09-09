5fba09856356b19d67534e58cb5ec2b385c1ce1d
Max Franz <maxkfranz@gmail.com>
2015-09-09 16:52:22 -0400

    remove third party layouts from listing

--- src/extensions/layout/index.js
+++ src/extensions/layout/index.js
@@ -1,18 +1,12 @@
 'use strict';
 
 module.exports = [
-  { name: 'arbor', impl: require('./arbor') },
   { name: 'breadthfirst', impl: require('./breadthfirst') },
   { name: 'circle', impl: require('./circle') },
-  { name: 'cola',impl: require('./cola') },
   { name: 'concentric',impl: require('./concentric') },
-  { name: 'cose2', impl: require('./cose2') },
   { name: 'cose', impl: require('./cose') },
-  { name: 'dagre', impl: require('./dagre') },
   { name: 'grid', impl: require('./grid') },
   { name: 'null', impl: require('./null') },
   { name: 'preset', impl: require('./preset') },
-  { name: 'random', impl: require('./random') },
-  { name: 'spread', impl: require('./spread') },
-  { name: 'springy', impl: require('./springy') }
+  { name: 'random', impl: require('./random') }
 ];

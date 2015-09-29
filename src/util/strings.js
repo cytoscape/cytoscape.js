7b0585d741a25eb14cb2c420a6bb0ab524242b33
maxkfranz <maxkfranz@gmail.com>
2015-09-29 19:09:02 -0400

    refactor

--- src/util/strings.js
+++ src/util/strings.js
@@ -6,49 +6,15 @@ var is = require('../is');
 module.exports = {
 
   camel2dash: memoize( function( str ){
-    var ret = [];
-
-    for( var i = 0; i < str.length; i++ ){
-      var ch = str[i];
-      var chLowerCase = ch.toLowerCase();
-      var isUpperCase = ch !== chLowerCase;
-
-      if( isUpperCase ){
-        ret.push( '-' );
-        ret.push( chLowerCase );
-      } else {
-        ret.push( ch );
-      }
-    }
-
-    var noUpperCases = ret.length === str.length;
-    if( noUpperCases ){ return str; } // cheaper than .join()
-
-    return ret.join('');
+    return str.replace(/([A-Z])/g, function( v ){
+      return '-' + v.toLowerCase();
+    });
   } ),
 
   dash2camel: memoize( function( str ){
-    var ret = [];
-    var nextIsUpper = false;
-
-    for( var i = 0; i < str.length; i++ ){
-      var ch = str[i];
-      var isDash = ch === '-';
-
-      if( isDash ){
-        nextIsUpper = true;
-      } else {
-        if( nextIsUpper ){
-          ret.push( ch.toUpperCase() );
-        } else {
-          ret.push( ch );
-        }
-
-        nextIsUpper = false;
-      }
-    }
-
-    return ret.join('');
+    return str.replace(/(-\w)/g, function( v ){
+      return v[1].toUpperCase();
+    });
   } ),
 
   capitalize: function(str){

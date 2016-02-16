239e55b28f1dce9537f5afec2afb836f436d5d66
Max Franz <maxkfranz@gmail.com>
2016-02-16 17:30:19 -0500

    indicate mit license title

--- src/preamble.js
+++ src/preamble.js
@@ -1,5 +1,7 @@
 /*!
-Copyright (c) 2016 The Cytoscape Consortium
+The MIT License
+
+Copyright (c) The Cytoscape Consortium
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the “Software”), to deal in

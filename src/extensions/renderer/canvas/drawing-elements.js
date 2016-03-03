f167ed7f6be2179eb741c59ed4c84688af5f8cfe
Max Franz <maxkfranz@gmail.com>
2016-03-03 16:44:49 -0500

    initial impl of layered texture caching #593

--- src/extensions/renderer/canvas/drawing-elements.js
+++ src/extensions/renderer/canvas/drawing-elements.js
@@ -4,23 +4,6 @@ var math = require( '../../../math' );
 
 var CRp = {};
 
-CRp.drawCachedElement = function( context, ele, pxRatio, extent ){
-  var r = this;
-  var _p = ele._private;
-  var rs = _p.rscratch;
-  var bb = ele.boundingBox();
-
-  if( math.boundingBoxesIntersect( bb, extent ) ){
-    var cache = r.data.eleTxrCache.getElement( ele, bb, pxRatio );
-
-    if( cache ){
-      context.drawImage( cache.texture.canvas, cache.x, 0, cache.width, cache.height, bb.x1, bb.y1, bb.w, bb.h );
-    } else { // if the element is not cacheable, then draw directly
-      r.drawElement( context, ele );
-    }
-  }
-};
-
 CRp.drawElement = function( context, ele, shiftToOriginWithBb ){
   var r = this;
 
@@ -31,14 +14,60 @@ CRp.drawElement = function( context, ele, shiftToOriginWithBb ){
   }
 };
 
+CRp.drawCachedElement = function( context, ele, pxRatio, extent ){
+  var r = this;
+  var _p = ele._private;
+  var rs = _p.rscratch;
+  var bb = ele.boundingBox();
+
+  if( !extent || math.boundingBoxesIntersect( bb, extent ) ){
+    var cache = r.data.eleTxrCache.getElement( ele, bb, pxRatio );
+
+    if( cache ){
+      context.drawImage( cache.texture.canvas, cache.x, 0, cache.width, cache.height, bb.x1, bb.y1, bb.w, bb.h );
+    } else { // if the element is not cacheable, then draw directly
+      r.drawElement( context, ele );
+    }
+  }
+};
+
 CRp.drawElements = function( context, eles, pxRatio, extent ){
   var r = this;
 
   for( var i = 0; i < eles.length; i++ ){
     var ele = eles[ i ];
 
-    // r.drawElement( context, ele );
+    r.drawElement( context, ele );
+  }
+};
+
+CRp.drawCachedElements = function( context, eles, pxRatio, extent ){
+  var r = this;
+
+  for( var i = 0; i < eles.length; i++ ){
+    var ele = eles[ i ];
+
     r.drawCachedElement( context, ele, pxRatio, extent );
+  }
+};
+
+CRp.drawLayeredElements = function( context, eles, pxRatio, extent ){
+  var r = this;
+  var cy = r.cy;
+  var pan = cy.pan();
+  var eles = cy.collection( eles );
+
+  var layers = r.data.lyrTxrCache.getLayers( eles, pxRatio );
+
+  if( layers ){
+    for( var i = 0; i < layers.length; i++ ){
+      var layer = layers[i];
+      var bb = layer.bb;
+
+      context.drawImage( layer.canvas, 0, 0, layer.width, layer.height, bb.x1, bb.y1, bb.w, bb.h );
+    }
+  } else { // fall back on plain caching if no layers
+    r.drawCachedElements( context, eles, pxRatio, extent );
   }
 };
 
--- src/extensions/renderer/canvas/layered-texture-cache.js
+++ src/extensions/renderer/canvas/layered-texture-cache.js
@@ -5,6 +5,10 @@ var util = require( '../../../util' );
 // TODO optimise these values
 
 var defNumLayers = 1; // default number of layers to use
+var minLvl = -4; // when scaling smaller than that we don't need to re-render
+var maxLvl = 2; // when larger than this scale just render directly (caching is not helpful)
+var maxZoom = 1.99; // beyond this zoom level, layered textures are not used
+var allowEdgeTxrCaching = true; // whether edges can be cached as textures (TODO maybe better on if webgl supported?)
 
 var LayeredTextureCache = function( renderer ){
   this.renderer = renderer;
@@ -13,18 +17,107 @@ var LayeredTextureCache = function( renderer ){
 
 var LTCp = LayeredTextureCache.prototype;
 
-LTCp.getLayers = function(){};
+LTCp.getLayers = function( eles, pxRatio, lvl ){
+  var r = this.renderer;
+  var cy = r.cy;
+  var zoom = cy.zoom();
 
-LTCp.invalidateElement = function(){};
+  if( lvl == null ){
+    lvl = Math.ceil( Math.log2( zoom * pxRatio ) );
 
+    if( lvl < minLvl ){
+      lvl = minLvl;
+    } else if( zoom >= maxZoom || lvl > maxLvl ){
+      return null;
+    }
+  }
+
+  var scale = Math.pow( 2, lvl );
+  var layers = this.layersByLevel[ lvl ] = this.layersByLevel[ lvl ] || [];
+  var bb;
+
+  var makeLayer = function( oldLayer ){
+    bb = bb || cy.collection( eles ).boundingBox();
+
+    var w = bb.w * scale;
+    var h = bb.h * scale;
+
+    var canvas = document.createElement('canvas');
+
+    canvas.width = w;
+    canvas.height = h;
+
+    var layer = {
+      bb: bb,
+      level: lvl,
+      width: w,
+      height: h,
+      canvas: canvas,
+      context: canvas.getContext('2d'),
+      eles: cy.collection()
+    };
+
+    var cxt = layer.context;
+    var dx = -layer.bb.x1;
+    var dy = -layer.bb.y1;
+
+    // do the transform on creation to save cycles (it's the same for all eles)
+    cxt.scale( scale, scale );
+    cxt.translate( dx, dy );
+
+    if( oldLayer != null ){ // put after old layer
+      var index = layers.indexOf( oldLayer ) + 1;
+
+      layers.splice( index, 0, layer );
+    } else {
+      layers.push( layer );
+    }
+
+    return layer;
+  };
+
+  var layer;
+  var maxElesPerLayer = eles.length / defNumLayers;
+  var needFreshLayerNextMiss = false;
+
+  for( var i = 0; i < eles.length; i++ ){
+    var ele = eles[i];
+    var rs = ele._private.rscratch;
+    var caches = rs.imgLayerCaches = rs.imgLayerCaches || {};
+
+    if( !caches[ lvl ] ){
+      if( !layer || needFreshLayerNextMiss || layer.eles.length >= maxElesPerLayer ){
+        layer = makeLayer( layer );
+        needFreshLayerNextMiss = false;
+      }
+
+      // r.drawElement( layer.context, ele, pxRatio );
+      // TODO invalidate when fresh ele caches are available
+      r.drawCachedElement( layer.context, ele, pxRatio );
+
+      layer.eles.merge( ele );
+
+      caches[ lvl ] = layer;
+    } else {
+      // if we have (ele not in cached layer) then (ele in cached layer), then we
+      // need to generate a fresh layer the next time we get another (ele not in cached layer)
+      layer = caches[ lvl ];
+      needFreshLayerNextMiss = true;
+    }
+  }
+
+  return layers;
+};
+
+LTCp.invalidateElement = function(){
+  // TODO
+};
+
+// maybe not needed? (simpler w/o)
 LTCp.retireTexture = function(){};
-
 LTCp.recycleTexture = function(){};
-
 LTCp.addTexture = function(){};
-
 LTCp.dequeueLayers = function(){};
-
 LTCp.setupDequeueing = function(){};
 
 

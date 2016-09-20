8e931765c96545d0ff26ca1af34302bbfbed5b48
Max Franz <maxkfranz@gmail.com>
2016-09-20 13:28:34 -0400

    Firefox JS engine throws exception on 0 dimension `c2d.drawImage()` calls #1529

--- src/extensions/renderer/canvas/ele-texture-cache.js
+++ src/extensions/renderer/canvas/ele-texture-cache.js
@@ -89,6 +89,8 @@ ETCp.getElement = function( ele, bb, pxRatio, lvl, reason ){
   var rs = ele._private.rscratch;
   var zoom = r.cy.zoom();
 
+  if( bb.w === 0 || bb.h === 0 ){ return null; }
+
   if( lvl == null ){
     lvl = Math.ceil( math.log2( zoom * pxRatio ) );
   }

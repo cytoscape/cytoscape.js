da628243db3052bd04d2c795a6044562c82c1310
Max Franz <maxkfranz@gmail.com>
2016-09-21 16:19:07 -0400

    Update for debug page with removed jq plugin #1539

--- debug/layout.js
+++ debug/layout.js
@@ -1,38 +1,29 @@
 $(function(){
 
-	$("#cytoscape").cy(function(){
-		var cy = this;
-
-		$("#layout-button").bind("click", function(){
-			cy.layout({
-				name: $("#layout-select").val()
-			}).run();
-		});
-
-		var start, end;
-		cy.bind("layoutstart", function(){
-			start = +new Date;
-		}).bind("layoutstop", function(){
-			end = +new Date;
-			var time = end - start;
-
-			if( !isNaN(time) ){
-				$("#layout-time").html( (time) + " ms" );
-			}
-		});
-
+	$("#layout-button").bind("click", function(){
+		cy.layout({
+			name: $("#layout-select").val()
+		}).run();
 	});
 
-	$("#cytoscape2").cy(function(){
-		var cy2 = this;
+	var start, end;
+	cy.bind("layoutstart", function(){
+		start = +new Date;
+	}).bind("layoutstop", function(){
+		end = +new Date;
+		var time = end - start;
 
-		$("#run-cose").on("click", function(){
-			cy2.layout({
-				name: 'cose'
-			}).run();
-		});
-
-
+		if( !isNaN(time) ){
+			$("#layout-time").html( (time) + " ms" );
+		}
 	});
+
+
+	$("#run-cose").on("click", function(){
+		cy2.layout({
+			name: 'cose'
+		}).run();
+	});
+
 
 });

1b23a486fca56e85817c5956717a76e811fec693
Max Franz <maxkfranz@gmail.com>
2016-10-29 14:36:43 +0100

    Node Background Image: Data URI images won't load on Safari #1582
    crossOrigin not set if data uri specified

--- src/extensions/renderer/base/images.js
+++ src/extensions/renderer/base/images.js
@@ -18,7 +18,15 @@ BRp.getCachedImage = function( url, onLoad ){
 
     var image = cache.image = new Image(); // eslint-disable-line no-undef
     image.addEventListener('load', onLoad);
-    image.crossOrigin = 'Anonymous'; // prevent tainted canvas
+
+    // #1582 safari doesn't load data uris with crossOrigin properly
+    // https://bugs.webkit.org/show_bug.cgi?id=123978
+    var dataUriPrefix = 'data:';
+    var isDataUri = url.substring( 0, dataUriPrefix.length ).toLowerCase() === dataUriPrefix;
+    if( !isDataUri ){
+      image.crossOrigin = 'Anonymous'; // prevent tainted canvas
+    }
+
     image.src = url;
 
     return image;

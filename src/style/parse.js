8afa8096734229003dca81061305eace747d56bb
Max Franz <maxkfranz@gmail.com>
2016-11-24 10:19:15 -0500

    Allow non-string values to be specified as string property values (e.g. `label`) #1620

--- src/style/parse.js
+++ src/style/parse.js
@@ -392,7 +392,7 @@ var parseImpl = function( name, value, propIsBypass, propIsFlat ){
     // just return
     return {
       name: name,
-      value: value,
+      value: '' + value,
       strValue: '' + value,
       bypass: propIsBypass
     };

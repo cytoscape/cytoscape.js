55861c5506e9d3efd2b82d834db12e7ba52220d3
Manfred Cheung <mj3cheun@uwaterloo.ca>
2017-04-18 15:09:26 -0400

    Refactored 6000-elements

--- documentation/demos/6000-elements/code.js
+++ documentation/demos/6000-elements/code.js
@@ -1,44 +1,29 @@
-var domReady = new Promise(function(resolve) {
-  window.addEventListener('DOMContentLoaded', function() {
-    resolve();
-  });
-});
-
-var fetchData = fetch('data.json', {mode: 'no-cors'})
+fetch('data.json', {mode: 'no-cors'})
   .then(function(res) {
     return res.json()
-  });
-
-var initCy = function(cy3json) {
-  return cytoscape({
-    container: document.getElementById('cy'),
-    elements: cy3json.elements,
-    layout: {
-      name: 'preset'
-    },
-    style: [{
-        selector: 'node',
-        style: {
-          'label': 'data(label)',
-          'background-color': '#aaa'
-        }
-      },
-      {
-        selector: 'edge',
-        style: {
-          'opacity': 0.2,
-          'line-color': '#ccc',
-          'width': 3
-        }
-      }
-    ]
-  });
-};
-
-Promise.all([fetchData, domReady])
-  .then(function(val) {
-    return initCy(val[0]);
   })
-  .then(function(cy) {
-    window.cy = cy; // put it in the window so you can play with the instance
+  .then(function(data) {
+    var cy = window.cy = cytoscape({
+      container: document.getElementById('cy'),
+      elements: data.elements,
+      layout: {
+        name: 'preset'
+      },
+      style: [{
+          selector: 'node',
+          style: {
+            'label': 'data(label)',
+            'background-color': '#aaa'
+          }
+        },
+        {
+          selector: 'edge',
+          style: {
+            'opacity': 0.2,
+            'line-color': '#ccc',
+            'width': 3
+          }
+        }
+      ]
+    });
   });

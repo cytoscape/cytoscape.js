52da6fb288f853d85e81f248bdfe604d34b9255a
Max Franz <maxkfranz@gmail.com>
2017-05-17 15:37:38 -0400

    File that tells mocha which src to use can't `require('process')` for node<4

--- src/test.js
+++ src/test.js
@@ -2,12 +2,9 @@
 This file tells the Mocha tests what build of Cytoscape to use.
 */
 
-var process = require('process');
-var env = process.env;
-
 // For Travis or manual build tests, use the CJS build
 // NB : Must do `gulp build-cjs` before `mocha` or `gulp-test`
-if( env.TRAVIS || env.TEST_BUILD ){
+if( process.env.TRAVIS || process.env.TEST_BUILD ){
   module.exports = require('../build/cytoscape.cjs.js');
 
 // Otherwise use the unbundled, unbabelified, raw source

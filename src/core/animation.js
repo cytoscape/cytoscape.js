165bdf194ebcd91fc9d9381f58af902ad5f17829
Max Franz <maxkfranz@gmail.com>
2017-07-06 15:13:23 -0400

    Spring animation should always end at 100% #1875

--- src/core/animation.js
+++ src/core/animation.js
@@ -688,6 +688,10 @@ var corefn = ({
     };
 
     function getEasedValue( type, start, end, percent, easingFn ){
+      if( percent === 1 ){
+        return end;
+      }
+
       var val = easingFn( start, end, percent );
 
       if( type == null ){

db8e3dbc724e58e9c83fcccccac42b2f8867001b
Alex Li <Alex Li>
2017-07-18 13:46:12 -0400

    changed gulpfile

--- benchmark/single/index.js
+++ benchmark/single/index.js
@@ -1,3 +1,3 @@
 // set this to run just a single suite via `gulp benchmark-single`
 // (useful when working on a specific function)
-module.exports = require('../add.js');
+module.exports = require('../classes.js');

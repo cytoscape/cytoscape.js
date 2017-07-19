e35f0e8f71a42cc6fda9e987ef6c7f4feacbb922
Max Franz <maxkfranz@gmail.com>
2017-07-19 13:14:17 -0400

    More complete Set internal polyfill

--- src/set.js
+++ src/set.js
@@ -1,5 +1,11 @@
-function ObjectSet(){
+/* global Set */
+
+function ObjectSet( iterable ){
   this._obj = {};
+
+  for( let val of iterable ){
+    this.add( val );
+  }
 }
 
 let p = ObjectSet.prototype;
@@ -16,8 +22,24 @@ p.has = function( val ){
   return this._obj[ val ] === 1;
 };
 
+p.values = function(){
+  return Object.keys( this._obj ).filter( key => this.has(key) );
+};
+
+p.entries = function(){
+  return this.values.map( val => [ val, val ] );
+};
+
+p.size = function(){
+  return this.values().length;
+};
+
+p.clear = function(){
+  this.values().forEach( val => this.remove(val) );
+};
+
 p.forEach = function( callback, thisArg ){
-  return Object.keys( this._obj ).forEach( callback, thisArg );
+  return this.values().forEach( callback, thisArg );
 };
 
 module.exports = typeof Set !== 'undefined' ? Set : ObjectSet;

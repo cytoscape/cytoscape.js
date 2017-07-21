4cdb4fde1bdbc2f9538fdf9586032ab7974e098f
Alexander Li <alex9630@gmail.com>
2017-07-21 14:43:08 -0400

    Changed copying of Set so polyfill works

--- benchmark/single/index.js
+++
@@ -1,3 +0,0 @@
-// set this to run just a single suite via `gulp benchmark-single`
-// (useful when working on a specific function)
-module.exports = require('../add.js');
--- debug/style.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	$("#string-stylesheet-apply-button").on("click", function(){
-		var stylesheetStr = $('#string-stylesheet').val();
-
-		cy.style().fromString( stylesheetStr ).update();
-	});	
-});

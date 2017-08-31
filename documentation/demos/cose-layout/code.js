6f4d06977929205b485dacb6a6d099d722e07495
Max Franz <maxkfranz@gmail.com>
2017-08-31 11:59:05 -0400

    Fix cose options in demo

--- documentation/demos/cose-layout/code.js
+++ documentation/demos/cose-layout/code.js
@@ -15,7 +15,20 @@ Promise.all([
       layout: {
         name: 'cose',
         idealEdgeLength: 100,
-        nodeOverlap: 20
+        nodeOverlap: 20,
+        refresh: 20,
+        fit: true,
+        padding: 30,
+        randomize: false,
+        componentSpacing: 100,
+        nodeRepulsion: 400000,
+        edgeElasticity: 100,
+        nestingFactor: 5,
+        gravity: 80,
+        numIter: 1000,
+        initialTemp: 200,
+        coolingFactor: 0.95,
+        minTemp: 1.0
       },
 
       style: dataArray[0],

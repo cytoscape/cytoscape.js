7be85648312d63f68d6d2db72937ed813133a03f
Max Franz <maxkfranz@gmail.com>
2017-11-10 17:37:24 -0500

    Edge points as rendered points #1986

--- src/collection/dimensions/edge-points.js
+++ src/collection/dimensions/edge-points.js
@@ -1,23 +1,56 @@
-let ifEdge = (self, then) => {
-  if( self.isEdge() ){
-    return then( self.renderer() );
+const math = require('../../math');
+
+const ifEdge = (ele, getValue) => {
+  if( ele.isEdge() ){
+    return getValue( ele );
   }
 };
 
-module.exports = {
-  controlPoints: function(){
-    return ifEdge( this, renderer => renderer.getControlPoints( this ) );
-  },
-  segmentPoints: function(){
-    return ifEdge( this, renderer => renderer.getSegmentPoints( this ) );
-  },
-  sourceEndpoint: function(){
-    return ifEdge( this, renderer => renderer.getSourceEndpoint( this ) );
-  },
-  targetEndpoint: function(){
-    return ifEdge( this, renderer => renderer.getTargetEndpoint( this ) );
-  },
-  midpoint: function(){
-    return ifEdge( this, renderer => renderer.getEdgeMidpoint( this ) );
+const ifEdgeRenderedPosition = (ele, getPoint) => {
+  if( ele.isEdge() ){
+    let cy = ele.cy();
+
+    return math.modelToRenderedPosition( getPoint( ele ), cy.zoom(), cy.pan() );
   }
 };
+
+const ifEdgeRenderedPositions = (ele, getPoints) => {
+  if( ele.isEdge() ){
+    let cy = ele.cy();
+    let pan = cy.pan();
+    let zoom = cy.zoom();
+
+    return getPoints( ele ).map( p => math.modelToRenderedPosition( p, zoom, pan ) );
+  }
+};
+
+const controlPoints = ele => ele.renderer().getControlPoints( ele );
+const segmentPoints = ele => ele.renderer().getSegmentPoints( ele );
+const sourceEndpoint = ele => ele.renderer().getSourceEndpoint( ele );
+const targetEndpoint = ele => ele.renderer().getTargetEndpoint( ele );
+const midpoint = ele => ele.renderer().getEdgeMidpoint( ele );
+
+const pts = {
+  controlPoints: { get: controlPoints, mult: true },
+  segmentPoints: { get: segmentPoints, mult: true },
+  sourceEndpoint: { get: sourceEndpoint },
+  targetEndpoint: { get: targetEndpoint },
+  midpoint: { get: midpoint }
+};
+
+const renderedName = name => 'rendered' + name[0].toUpperCase() + name.substr(1);
+
+module.exports = Object.keys( pts ).reduce( ( obj, name ) => {
+  let spec = pts[ name ];
+  let rName = renderedName( name );
+
+  obj[ name ] = function(){ return ifEdge( this, spec.get ); };
+
+  if( spec.mult ){
+    obj[ rName ] = function(){ return ifEdgeRenderedPositions( this, spec.get ); };
+  } else {
+    obj[ rName ] = function(){ return ifEdgeRenderedPosition( this, spec.get ); };
+  }
+
+  return obj;
+}, {} );

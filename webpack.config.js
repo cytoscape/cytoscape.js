2f29cf5e7df9fdf22686b6d9449b7d2e80238488
Max Franz <maxkfranz@gmail.com>
2018-01-24 14:11:03 -0500

    Update to webpack@3 and disable all webpack node polyfills #2051

--- webpack.config.js
+++ webpack.config.js
@@ -53,5 +53,6 @@ module.exports = {
       }
     }) : null
   ].filter( isNotNil ),
-  devtool: SOURCEMAPS ? 'inline-source-map' : false
+  devtool: SOURCEMAPS ? 'inline-source-map' : false,
+  node: false
 };

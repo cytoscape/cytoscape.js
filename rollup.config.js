9bb5440d6cfea5ce1953df9feb5dee0f45f6d443
Bogdan Chadkin <trysound@yandex.ru>
2018-05-06 15:55:58 +0300

    Provide more strict output formats and esm
    Renamed `cytoscape.js` to `cytoscape.umd.js` as a more descriptive name.
    `umd` is usually used for self-sufficient files without external
    dependencies or with only framework specific dependencies like `angular`
    or `react`.
    
    Replace `NODE_ENV` with `production` for `cytoscape.min.js` because
    minified files usually should be ready for production. This is usual
    convention for this name across many packages.
    
    Allow users to choose development/production mode for its dependencies
    by not replacing `NODE_ENV` for "main" bundle. Currently `webpack` has
    own modes to provide this replacement. Rollup users are usually cares
    about with with `rollup-plugin-replace` plugin.
    
    As a result we stop supporting web (iife and amd) in "main" bundle since
    it always requires a build step. So I changed its format to `cjs` and
    renamed to `cytoscape.cjs.js`. This also make me able to reduce some
    repition with its config.
    
    Added `cytoscape.esm.js` bundle with similar to `cjs` configuration.
    Users may consume it from package.json `module` field.
    
    So now we get "dist" pattern which is used across many modern frontend
    packages.
    
    ```
    cytoscape.cjs.js
    cytoscape.esm.js
    cytoscape.min.js
    cytoscape.umd.js
    ```
    
    Added size snapshot rollup plugin to track size changes on every commit.
    For `esm` bundle it also computes treeshaked size with both rollup and
    webpack so you can use it for the future decomposing library to let
    bundle treeshake out unused things.

--- rollup.config.js
+++ rollup.config.js
@@ -3,27 +3,34 @@ import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import replace from 'rollup-plugin-replace';
 import uglify from 'rollup-plugin-uglify';
-import pkg from './package.json';
+import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
 
 const FILE = process.env.FILE;
 const SOURCEMAPS = process.env.SOURCEMAPS === 'true'; // default false
 const BABEL = process.env.BABEL !== 'false'; // default true
-const VERSION = process.env.VERSION || 'snapshot';
 
 const input = './src/index.js';
 
 const name = 'cytoscape';
 
+const envVariables = {
+  'process.env.VERSION': JSON.stringify(process.env.VERSION || 'snapshot')
+}
+
 const getBabelOptions = () => ({
   exclude: '**/node_modules/**',
+  // inject in the bundle babel helpers
   plugins: ['external-helpers']
 });
+
+// Ignore all node_modules dependencies
+const isExternal = id => !id.startsWith('\0') && !id.startsWith('.') && !id.startsWith('/');
 
 const configs = [
   {
     input,
     output: {
-      file: 'build/cytoscape.js',
+      file: 'build/cytoscape.umd.js',
       format: 'umd',
       name,
       sourcemap: SOURCEMAPS ? 'inline' : false
@@ -32,10 +39,10 @@ const configs = [
       nodeResolve(),
       commonjs({ include: '**/node_modules/**' }),
       BABEL ? babel(getBabelOptions()) : {},
-      replace({
-        'process.env.NODE_ENV': JSON.stringify('development'),
-        'process.env.VERSION': JSON.stringify(VERSION)
-      })
+      replace(Object.assign({}, envVariables, {
+        'process.env.NODE_ENV': JSON.stringify('development')
+      })),
+      !FILE ? sizeSnapshot() : {}
     ]
   },
 
@@ -50,10 +57,10 @@ const configs = [
       nodeResolve(),
       commonjs({ include: '**/node_modules/**' }),
       BABEL ? babel(getBabelOptions()) : {},
-      replace({
-        'process.env.NODE_ENV': JSON.stringify('development'),
-        'process.env.VERSION': JSON.stringify(VERSION)
-      }),
+      replace(Object.assign({}, envVariables, {
+        'process.env.NODE_ENV': JSON.stringify('production')
+      })),
+      !FILE ? sizeSnapshot() : {},
       uglify({
         compress: {
           warnings: false,
@@ -65,25 +72,25 @@ const configs = [
 
   {
     input,
-    output: {
-      file: 'build/cytoscape.umd.js',
-      format: 'umd',
-      name,
-      sourcemap: SOURCEMAPS ? 'inline' : false,
-      globals: {
-        'heap': 'Heap',
-        'lodash.debounce': '_.debounce'
-      }
-    },
-    external: Object.keys( pkg.dependencies || {} ),
+    output: { file: 'build/cytoscape.cjs.js', format: 'cjs' },
+    external: isExternal,
     plugins: [
       nodeResolve(),
-      commonjs({ include: '**/node_modules/**' }),
       BABEL ? babel(getBabelOptions()) : {},
-      replace({
-        'process.env.NODE_ENV': JSON.stringify('production'),
-        'process.env.VERSION': JSON.stringify(VERSION)
-      })
+      replace(envVariables),
+      !FILE ? sizeSnapshot() : {}
+    ]
+  },
+
+  {
+    input,
+    output: { file: 'build/cytoscape.esm.js', format: 'es' },
+    external: isExternal,
+    plugins: [
+      nodeResolve(),
+      BABEL ? babel(getBabelOptions()) : {},
+      replace(envVariables),
+      !FILE ? sizeSnapshot() : {}
     ]
   }
 ];

d70acfc404ca18059478dc2eb252a2a788596781
Max Franz <maxkfranz@gmail.com>
2018-07-23 15:27:52 -0400

    Improve performance of A*
    - Replace `var` with `let`
    - Use a heap for the node queue
    - Cheaper check for in open set & helper functions
    - Reduce id() calls and use a map for faster closed set lookup
    - Build resultant path without recursion
    - Improves performance by ~2.1x on GAL
    
    Ref #1224

--- src/collection/algorithms/a-star.js
+++ src/collection/algorithms/a-star.js
@@ -1,62 +1,19 @@
 import * as is from '../../is';
+import Heap from '../../heap';
+import Set from '../../set';
 
-var elesfn = ({
+let elesfn = ({
 
   // Implemented from pseudocode from wikipedia
   aStar: function( options ){
-    var eles = this;
-
     options = options || {};
 
-    // Reconstructs the path from Start to End, acumulating the result in pathAcum
-    var reconstructPath = function( start, end, cameFromMap, pathAcum ){
-      // Base case
-      if( start == end ){
-        pathAcum.unshift( cy.getElementById( end ) );
-        return pathAcum;
-      }
-
-      if( end in cameFromMap ){
-        // We know which node is before the last one
-        var previous = cameFromMap[ end ];
-        var previousEdge = cameFromEdge[ end ];
-
-        pathAcum.unshift( cy.getElementById( previousEdge ) );
-        pathAcum.unshift( cy.getElementById( end ) );
-
-        return reconstructPath( start,
-                     previous,
-                     cameFromMap,
-                     pathAcum );
-      }
-
-      // We should not reach here!
-      return undefined;
-    };
-
-    // Returns the index of the element in openSet which has minimum fScore
-    var findMin = function( openSet, fScore ){
-      if( openSet.length === 0 ){
-        // Should never be the case
-        return undefined;
-      }
-      var minPos = 0;
-      var tempScore = fScore[ openSet[0] ];
-      for( var i = 1; i < openSet.length; i++ ){
-        var s = fScore[ openSet[ i ] ];
-        if( s < tempScore ){
-          tempScore = s;
-          minPos = i;
-        }
-      }
-      return minPos;
-    };
-
-    var cy = this._private.cy;
+    let cy = this._private.cy;
 
     // root - mandatory!
+    let source;
     if( options != null && options.root != null ){
-      var source = is.string( options.root ) ?
+      source = is.string( options.root ) ?
         // use it as a selector, e.g. "#rootID
         this.filter( options.root )[0] :
         options.root[0];
@@ -65,8 +22,9 @@ var elesfn = ({
     }
 
     // goal - mandatory!
+    let target;
     if( options.goal != null ){
-      var target = is.string( options.goal ) ?
+      target = is.string( options.goal ) ?
         // use it as a selector, e.g. "#goalID
         this.filter( options.goal )[0] :
         options.goal[0];
@@ -75,73 +33,108 @@ var elesfn = ({
     }
 
     // Heuristic function - optional
+    let heuristic;
     if( options.heuristic != null && is.fn( options.heuristic ) ){
-      var heuristic = options.heuristic;
+      heuristic = options.heuristic;
     } else {
-      var heuristic = function(){ return 0; }; // use constant if unspecified
+      heuristic = function(){ return 0; }; // use constant if unspecified
     }
 
     // Weight function - optional
+    let weightFn;
     if( options.weight != null && is.fn( options.weight ) ){
-      var weightFn = options.weight;
+      weightFn = options.weight;
     } else {
       // If not specified, assume each edge has equal weight (1)
-      var weightFn = function( e ){return 1;};
+      weightFn = function( e ){return 1;};
     }
 
     // directed - optional
+    let directed;
     if( options.directed != null ){
-      var directed = options.directed;
+      directed = options.directed;
     } else {
-      var directed = false;
+      directed = false;
     }
 
-    var sid = source.id();
-    var tid = target.id();
+    let sid = source.id();
+    let tid = target.id();
 
-    var closedSet = [];
-    var openSet = [ sid ];
-    var cameFrom = {};
-    var cameFromEdge = {};
-    var gScore = {};
-    var fScore = {};
+    let gScore = {};
+    let fScore = {};
+    let closedSetIds = {};
+    let openSet = new Heap( (a, b) => fScore[a.id()] - fScore[b.id()] );
+    let openSetIds = new Set();
+    let cameFrom = {};
+    let cameFromEdge = {};
+
+    let addToOpenSet = (ele, id) => {
+      openSet.push(ele);
+      openSetIds.add(id);
+    };
+
+    let cMin, cMinId;
+
+    let popFromOpenSet = () => {
+      cMin = openSet.pop();
+      cMinId = cMin.id();
+      openSetIds.delete(cMinId);
+    };
+
+    let isInOpenSet = id => openSetIds.has(id);
+
+    addToOpenSet(source, sid);
 
     gScore[ sid ] = 0;
     fScore[ sid ] = heuristic( source );
 
     // Counter
-    var steps = 0;
+    let steps = 0;
 
     // Main loop
-    while( openSet.length > 0 ){
-      var minPos = findMin( openSet, fScore );
-      var cMin = cy.getElementById( openSet[ minPos ] );
-      var cMinId = cMin.id();
+    while( openSet.size() > 0 ){
+      popFromOpenSet();
       steps++;
 
       // If we've found our goal, then we are done
-      if( cMinId == tid ){
-        var rPath = reconstructPath( sid, tid, cameFrom, [] );
+      if( cMinId === tid ){
+        let path = [];
+        let pathNode = target;
+        let pathNodeId = tid;
+        let pathEdge = cameFromEdge[pathNodeId];
+
+        for( ;; ){
+          path.unshift(pathNode);
+
+          if( pathEdge != null ){
+            path.unshift(pathEdge);
+          }
+
+          pathNode = cameFrom[pathNodeId];
+
+          if( pathNode == null ){ break; }
+
+          pathNodeId = pathNode.id();
+          pathEdge = cameFromEdge[pathNodeId];
+        }
 
         return {
           found: true,
           distance: gScore[ cMinId ],
-          path: eles.spawn( rPath ),
-          steps: steps
+          path: this.spawn( path ),
+          steps
         };
       }
 
       // Add cMin to processed nodes
-      closedSet.push( cMinId );
-      // Remove cMin from boundary nodes
-      openSet.splice( minPos, 1 );
+      closedSetIds[ cMinId ] = true;
 
       // Update scores for neighbors of cMin
       // Take into account if graph is directed or not
-      var vwEdges = cMin._private.edges;
+      let vwEdges = cMin._private.edges;
 
-      for( var i = 0; i < vwEdges.length; i++ ){
-        var e = vwEdges[ i ];
+      for( let i = 0; i < vwEdges.length; i++ ){
+        let e = vwEdges[ i ];
 
         // edge must be in set of calling eles
         if( !this.hasElementWithId( e.id() ) ){ continue; }
@@ -149,22 +142,22 @@ var elesfn = ({
         // cMin must be the source of edge if directed
         if( directed && e.data('source') !== cMinId ){ continue; }
 
-        var wSrc = e.source();
-        var wTgt = e.target();
+        let wSrc = e.source();
+        let wTgt = e.target();
 
-        var w = wSrc.id() !== cMinId ? wSrc : wTgt;
-        var wid = w.id();
+        let w = wSrc.id() !== cMinId ? wSrc : wTgt;
+        let wid = w.id();
 
         // node must be in set of calling eles
         if( !this.hasElementWithId( wid ) ){ continue; }
 
         // if node is in closedSet, ignore it
-        if( closedSet.indexOf( wid ) != -1 ){
+        if( closedSetIds[ wid ] ){
           continue;
         }
 
         // New tentative score for node w
-        var tempScore = gScore[ cMinId ] + weightFn( e );
+        let tempScore = gScore[ cMinId ] + weightFn( e );
 
         // Update gScore for node w if:
         //   w not present in openSet
@@ -172,19 +165,21 @@ var elesfn = ({
         //   tentative gScore is less than previous value
 
         // w not in openSet
-        if( openSet.indexOf( wid ) == -1 ){
+        if( !isInOpenSet(wid) ){
           gScore[ wid ] = tempScore;
           fScore[ wid ] = tempScore + heuristic( w );
-          openSet.push( wid ); // Add node to openSet
-          cameFrom[ wid ] = cMinId;
-          cameFromEdge[ wid ] = e.id();
+          addToOpenSet( w, wid );
+          cameFrom[ wid ] = cMin;
+          cameFromEdge[ wid ] = e;
+
           continue;
         }
+
         // w already in openSet, but with greater gScore
         if( tempScore < gScore[ wid ] ){
           gScore[ wid ] = tempScore;
           fScore[ wid ] = tempScore + heuristic( w );
-          cameFrom[ wid ] = cMinId;
+          cameFrom[ wid ] = cMin;
         }
 
       } // End of neighbors update

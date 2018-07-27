8c39abbeed99f1f099bc06c975144341307daf20
Max Franz <maxkfranz@gmail.com>
2018-07-27 15:54:51 -0400

    Optimisations for Page Rank
    - Use `util.defaults()`
    - Use general normalisation function in `math`
    - Use single dimensional array for N*N matrix
    - Declare array sizes on init and do not change array lengths
    - Re-use arrays that represent the vectors of the main loop of the alg. -- better memory usage & less GC
    - Improves benchmark on GAL graph by ~1.5x
    
    Ref #1224

--- src/collection/algorithms/page-rank.js
+++ src/collection/algorithms/page-rank.js
@@ -1,164 +1,125 @@
-import * as is from '../../is';
+import { defaults } from '../../util';
+import { inPlaceSumNormalize } from '../../math';
+
+const pageRankDefaults = defaults({
+  dampingFactor: 0.8,
+  precision: 0.000001,
+  iterations: 200,
+  weight: edge => 1
+});
 
 let elesfn = ({
 
   pageRank: function( options ){
-    options = options || {};
-
-    let normalizeVector = function( vector ){
-      let length = vector.length;
-
-      // First, get sum of all elements
-      let total = 0;
-      for( let i = 0; i < length; i++ ){
-        total += vector[ i ];
-      }
-
-      // Now, divide each by the sum of all elements
-      for( let i = 0; i < length; i++ ){
-        vector[ i ] = vector[ i ] / total;
-      }
-    };
-
-    // dampingFactor - optional
-    let dampingFactor;
-    if( options != null &&
-      options.dampingFactor != null ){
-      dampingFactor = options.dampingFactor;
-    } else {
-      dampingFactor = 0.8; // Default damping factor
-    }
-
-    // desired precision - optional
-    let epsilon;
-    if( options != null &&
-      options.precision != null ){
-      epsilon = options.precision;
-    } else {
-      epsilon = 0.000001; // Default precision
-    }
-
-    // Max number of iterations - optional
-    let numIter;
-    if( options != null &&
-      options.iterations != null ){
-      numIter = options.iterations;
-    } else {
-      numIter = 200; // Default number of iterations
-    }
-
-    // Weight function - optional
-    let weightFn;
-    if( options != null && options.weight != null && is.fn( options.weight ) ){
-      weightFn = options.weight;
-    } else {
-      // If not specified, assume each edge has equal weight (1)
-      weightFn = () => 1;
-    }
-
+    let { dampingFactor, precision, iterations, weight } = pageRankDefaults(options);
     let cy = this._private.cy;
     let { nodes, edges } = this.byGroup();
     let numNodes = nodes.length;
+    let numNodesSqd = numNodes * numNodes;
     let numEdges = edges.length;
-
-    // Create numerical identifiers for each node
-    // mapping: node id -> position in nodes array
-    // for reverse mapping, simply use nodes array
-    let id2position = {};
-    for( let i = 0; i < numNodes; i++ ){
-      id2position[ nodes[ i ].id() ] = i;
-    }
 
     // Construct transposed adjacency matrix
     // First lets have a zeroed matrix of the right size
     // We'll also keep track of the sum of each column
-    let matrix = [];
-    let columnSum = [];
+    let matrix = new Array(numNodesSqd);
+    let columnSum = new Array(numNodes);
     let additionalProb = (1 - dampingFactor) / numNodes;
 
     // Create null matrix
     for( let i = 0; i < numNodes; i++ ){
-      let newRow = [];
       for( let j = 0; j < numNodes; j++ ){
-        newRow.push( 0.0 );
+        let n = i * numNodes + j;
+
+        matrix[n] = 0;
       }
-      matrix.push( newRow );
-      columnSum.push( 0.0 );
+
+      columnSum[i] = 0;
     }
 
     // Now, process edges
     for( let i = 0; i < numEdges; i++ ){
       let edge = edges[ i ];
+      let srcId = edge.data('source');
+      let tgtId = edge.data('target');
 
       // Don't include loops in the matrix
-      if( edge.isLoop() ){ continue; }
+      if( srcId === tgtId ){ continue; }
 
-      let s = id2position[ edge.data('source') ];
-      let t = id2position[ edge.data('target') ];
-      let w = weightFn( edge );
+      let s = nodes.indexOfId( srcId );
+      let t = nodes.indexOfId( tgtId );
+      let w = weight( edge );
+      let n = t * numNodes + s;
 
       // Update matrix
-      matrix[ t ][ s ] += w;
+      matrix[n] += w;
 
       // Update column sum
-      columnSum[ s ] += w;
+      columnSum[s] += w;
     }
 
     // Add additional probability based on damping factor
     // Also, take into account columns that have sum = 0
     let p = 1.0 / numNodes + additionalProb; // Shorthand
+
     // Traverse matrix, column by column
     for( let j = 0; j < numNodes; j++ ){
-      if( columnSum[ j ] === 0 ){
+      if( columnSum[j] === 0 ){
         // No 'links' out from node jth, assume equal probability for each possible node
         for( let i = 0; i < numNodes; i++ ){
-          matrix[ i ][ j ] = p;
+          let n = i * numNodes + j;
+          matrix[n] = p;
         }
       } else {
         // Node jth has outgoing link, compute normalized probabilities
         for( let i = 0; i < numNodes; i++ ){
-          matrix[ i ][ j ] = matrix[ i ][ j ] / columnSum[ j ] + additionalProb;
+          let n = i * numNodes + j;
+
+          matrix[n] = matrix[n] / columnSum[j] + additionalProb;
         }
       }
     }
 
     // Compute dominant eigenvector using power method
-    let eigenvector = [];
-    let nullVector = [];
+    let eigenvector = new Array(numNodes);
+    let temp = new Array(numNodes);
     let previous;
 
     // Start with a vector of all 1's
     // Also, initialize a null vector which will be used as shorthand
     for( let i = 0; i < numNodes; i++ ){
-      eigenvector.push( 1.0 );
-      nullVector.push( 0.0 );
+      eigenvector[i] = 1;
     }
 
-    for( let iter = 0; iter < numIter; iter++ ){
-      // New array with all 0's
-      let temp = nullVector.slice( 0 );
+    for( let iter = 0; iter < iterations; iter++ ){
+      // Temp array with all 0's
+      for( let i = 0; i < numNodes; i++ ){
+        temp[i] = 0;
+      }
 
       // Multiply matrix with previous result
       for( let i = 0; i < numNodes; i++ ){
         for( let j = 0; j < numNodes; j++ ){
-          temp[ i ] += matrix[ i ][ j ] * eigenvector[ j ];
+          let n = i * numNodes + j;
+
+          temp[i] += matrix[n] * eigenvector[j];
         }
       }
 
-      normalizeVector( temp );
+      inPlaceSumNormalize( temp );
       previous = eigenvector;
       eigenvector = temp;
+      temp = previous;
 
       let diff = 0;
       // Compute difference (squared module) of both vectors
       for( let i = 0; i < numNodes; i++ ){
-        let delta = previous[ i ] - eigenvector[ i ];
+        let delta = previous[i] - eigenvector[i];
 
         diff += delta * delta;
       }
 
       // If difference is less than the desired threshold, stop iterating
-      if( diff < epsilon ){
+      if( diff < precision ){
         break;
       }
     }
@@ -166,16 +127,9 @@ let elesfn = ({
     // Construct result
     let res = {
       rank: function( node ){
-        let nodeId;
+        node = cy.collection(node)[0];
 
-        if( is.string( node ) ){
-          // is a selector string
-          nodeId = (cy.filter( node )[0]).id();
-        } else {
-          // is a node object
-          nodeId = node.id();
-        }
-        return eigenvector[ id2position[ nodeId ] ];
+        return eigenvector[ nodes.indexOf(node) ];
       }
     };
 

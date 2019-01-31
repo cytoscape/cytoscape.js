6c787c6f484df1aa7533a4866c551b317000404a
Xavier Ho <contact@xavierho.com>
2019-02-01 10:09:40 +1100

    Check for animation status before printing edge is impossible to draw warning that may be out of date

--- src/extensions/renderer/base/coord-ele-math/edge-control-points.js
+++ src/extensions/renderer/base/coord-ele-math/edge-control-points.js
@@ -126,9 +126,10 @@ BRp.storeAllpts = function( edge ){
 
 BRp.checkForInvalidEdgeWarning = function( edge ){
   var rs = edge._private.rscratch;
+  var cy = this.cy;
 
   if( !is.number(rs.startX) || !is.number(rs.startY) || !is.number(rs.endX) || !is.number(rs.endY) ){
-    if( !rs.loggedErr ){
+    if( !cy.animated() && !rs.loggedErr ){
       rs.loggedErr = true;
       util.warn('Edge `' + edge.id() + '` has invalid endpoints and so it is impossible to draw.  Adjust your edge style (e.g. control points) accordingly or use an alternative edge type.  This is expected behaviour when the source node and the target node overlap.');
     }

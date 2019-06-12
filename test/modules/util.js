eefadd6460b5ab138425bddb00356d722851a416
Max Franz <maxkfranz@gmail.com>
2019-06-12 18:12:15 -0400

    Work-in-progress tests to improve hashing robustness

--- test/modules/util.js
+++ test/modules/util.js
@@ -1,5 +1,9 @@
 import { expect } from 'chai';
-import { hashString, hashInt, hashIntsArray } from '../../src/util';
+import { hashString, hashInt, hashIntsArray, hashStrings } from '../../src/util';
+
+var randInt = function(min, max){
+  return Math.round(Math.random() * (max - min) + min);
+};
 
 describe('util', function(){
 
@@ -53,6 +57,170 @@ describe('util', function(){
 
         hashes[h] = true;
       }
+    });
+
+    it('hash is different for negative numbers', function(){
+      for( var i = 1; i < 1000; i++ ){
+        var h = hashInt(i);
+        var hn = hashInt(-i);
+
+        expect(h, 'hash '+i).to.not.equal(hn, 'hash -'+i);
+      }
+    });
+
+    if( process.env.TRAVIS ){
+      // run this test only on travis because it's slow
+
+      it('hash is unique per two-uint combos on 0...1000', function(){
+        this.timeout(5 * 60 * 1000);
+
+        var min = 0;
+        var max = 1000;
+        var hashes = {};
+        var i, j, h, arr = [0, 0];
+
+        for( i = min; i <= max; i++ ){
+          for( j = min; j <= max; j++ ){
+            arr[0] = i;
+            arr[1] = j;
+
+            h = hashIntsArray(arr);
+
+            expect(hashes[h]).to.not.exist;
+
+            hashes[h] = true;
+          }
+        }
+      });
+    }
+
+    it('hash is unique for random int pairs', function(){
+      var N = 100000;
+      var min = 0;
+      var max = 100;
+      var hashes = {};
+      var i, h, arr = [0, 0];
+      var existing;
+      var a, b, A, B;
+
+      for( i = 0; i < N; i++ ){
+        arr = [randInt(min, max), randInt(min, max)];
+
+        h = hashIntsArray(arr);
+
+        existing = hashes[h];
+
+        if( existing != null ){
+          a = arr[0];
+          b = arr[1];
+          A = existing.arr[0];
+          B = existing.arr[1];
+
+          if( (a !== A || b !== B) ){
+            throw new Error(a + ',' + b + ' collides with ' + A + ',' + B);
+          }
+        }
+
+        hashes[h] = { arr: arr, val: h };
+      }
+    });
+
+    function testMTuple(M){
+      it('hash is unique for random int ' + M + '-tuple', function(){
+        var N = 100000;
+        var min1 = 0;
+        var max1 = 100;
+        var min2 = 2147483647 - 1024;
+        var max2 = 2147483647;
+        var hashes = {};
+        var i, h, arr = [0, 0];
+        var existing;
+        var t1, t2;
+        var low;
+  
+        var getTupleString = function(arr){ return arr.join(','); };
+  
+        for( i = 0; i < N; i++ ){
+          arr = [];
+
+          for( var j = 0; j < M; j++ ){
+            low = Math.random() < 0.8;
+            arr.push( low ? randInt(min1, max1) : randInt(min2, max2) );
+          }
+  
+          h = hashIntsArray(arr);
+  
+          existing = hashes[h];
+  
+          if( existing != null ){
+            t1 = getTupleString(arr);
+            t2 = getTupleString(existing.arr);
+  
+            console.log(t1, t2)
+
+            if( t1 !== t2 ){
+              throw new Error(t1 + ' matches ' + t2 + ' with value ' + h);
+            }
+          }
+  
+          hashes[h] = { arr: arr, val: h };
+        }
+      });
+    }
+
+    (function(){
+      for( var i = 3; i <= 10; i++ ){
+        testMTuple(i);
+      }
+    })(); 
+
+    it('hash is unique for random low-high int pairs', function(){
+      var N = 1000;
+      var min1 = 0;
+      var max1 = 100;
+      var min2 = 2147483647 - 1024;
+      var max2 = 2147483647;
+      var hashes = {};
+      var i, h, arr = [0, 0];
+      var existing;
+      var a, b, A, B;
+
+      for( i = 0; i < N; i++ ){
+        arr = [randInt(min1, max1), randInt(min2, max2)];
+
+        h = hashIntsArray(arr);
+
+        existing = hashes[h];
+
+        if( existing != null ){
+          a = arr[0];
+          b = arr[1];
+          A = existing.arr[0];
+          B = existing.arr[1];
+
+          if( (a !== A || b !== B) ){
+            throw new Error(a + ',' + b + ' collides with ' + A + ',' + B);
+          }
+        }
+
+        hashes[h] = { arr: arr, val: h };
+      }
+    });
+
+    it('hash is unique for simulated style', function(){
+      var h1 = hashString('ellipse');
+      h1 = hashInt(30, h1);
+      h1 = hashInt(30, h1);
+      h1 = hashString('blue', h1);
+
+      var h2 = hashString('ellipse');
+      h2 = hashInt(35, h2);
+      h2 = hashInt(35, h2);
+      h2 = hashString('red', h2);
+      h2 = hashInt(2, h2);
+      h2 = hashString('green', h2);
+
+      expect(h1).to.not.equal(h2);
     });
   });
 

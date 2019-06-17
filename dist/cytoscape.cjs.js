2fa5b19be5317f211b4fd3fa0a0abc9383c2c0c6
Max Franz <maxkfranz@gmail.com>
2019-06-17 14:07:09 -0400

    Build 3.6.5

--- dist/cytoscape.cjs.js
+++ dist/cytoscape.cjs.js
@@ -20154,8 +20154,8 @@ var separateComponents = function separateComponents(layoutInfo, options) {
       var n = c[j];
 
       if (!n.isLocked) {
-        n.positionX += x;
-        n.positionY += y;
+        n.positionX += x - c.x1;
+        n.positionY += y - c.y1;
       }
     }
 
@@ -21908,7 +21908,28 @@ BRp$3.findEdgeControlPoints = function (edges) {
   var r = this;
   var cy = r.cy;
   var hasCompounds = cy.hasCompoundNodes();
-  var hashTable = new Map$1();
+  var hashTable = {
+    map: new Map$1(),
+    get: function get(pairId) {
+      var map2 = this.map.get(pairId[0]);
+
+      if (map2 != null) {
+        return map2.get(pairId[1]);
+      } else {
+        return null;
+      }
+    },
+    set: function set(pairId, val) {
+      var map2 = this.map.get(pairId[0]);
+
+      if (map2 == null) {
+        map2 = new Map$1();
+        this.map.set(pairId[0], map2);
+      }
+
+      map2.set(pairId[1], val);
+    }
+  };
   var pairIds = [];
   var haystackEdges = []; // create a table of edge (src, tgt) => list of edges between them
 
@@ -21929,13 +21950,11 @@ BRp$3.findEdgeControlPoints = function (edges) {
 
     var edgeIsUnbundled = curveStyle === 'unbundled-bezier' || curveStyle === 'segments' || curveStyle === 'straight' || curveStyle === 'taxi';
     var edgeIsBezier = curveStyle === 'unbundled-bezier' || curveStyle === 'bezier';
-
-    var srcIndex = _p.source.poolIndex();
-
-    var tgtIndex = _p.target.poolIndex();
-
-    var hash = (edgeIsUnbundled ? -1 : 1) * hashIntsArray([srcIndex, tgtIndex].sort());
-    var pairId = hash;
+    var src = _p.source;
+    var tgt = _p.target;
+    var srcIndex = src.poolIndex();
+    var tgtIndex = tgt.poolIndex();
+    var pairId = [srcIndex, tgtIndex].sort();
     var tableEntry = hashTable.get(pairId);
 
     if (tableEntry == null) {
@@ -27591,7 +27610,7 @@ CRp$1.drawElementOverlay = function (context, ele) {
   }
 };
 
-CRp$1.drawCachedElementPortion = function (context, ele, eleTxrCache, pxRatio, lvl, reason, getRotation) {
+CRp$1.drawCachedElementPortion = function (context, ele, eleTxrCache, pxRatio, lvl, reason, getRotation, getOpacity) {
   var r = this;
   var bb = eleTxrCache.getBoundingBox(ele);
 
@@ -27603,7 +27622,7 @@ CRp$1.drawCachedElementPortion = function (context, ele, eleTxrCache, pxRatio, l
   var eleCache = eleTxrCache.getElement(ele, bb, pxRatio, lvl, reason);
 
   if (eleCache != null) {
-    var opacity = ele.effectiveOpacity();
+    var opacity = getOpacity(r, ele);
 
     if (opacity === 0) {
       return;
@@ -27678,6 +27697,14 @@ var getTargetLabelRotation = function getTargetLabelRotation(r, ele) {
   return r.getTextAngle(ele, 'target');
 };
 
+var getOpacity = function getOpacity(r, ele) {
+  return ele.effectiveOpacity();
+};
+
+var getTextOpacity = function getTextOpacity(e, ele) {
+  return ele.pstyle('text-opacity').pfValue * ele.effectiveOpacity();
+};
+
 CRp$1.drawCachedElement = function (context, ele, pxRatio, extent, lvl, requestHighQuality) {
   var r = this;
   var _r$data = r.data,
@@ -27693,12 +27720,12 @@ CRp$1.drawCachedElement = function (context, ele, pxRatio, extent, lvl, requestH
   }
 
   if (!extent || boundingBoxesIntersect(bb, extent)) {
-    r.drawCachedElementPortion(context, ele, eleTxrCache, pxRatio, lvl, reason, getZeroRotation);
-    r.drawCachedElementPortion(context, ele, lblTxrCache, pxRatio, lvl, reason, getLabelRotation);
+    r.drawCachedElementPortion(context, ele, eleTxrCache, pxRatio, lvl, reason, getZeroRotation, getOpacity);
+    r.drawCachedElementPortion(context, ele, lblTxrCache, pxRatio, lvl, reason, getLabelRotation, getTextOpacity);
 
     if (ele.isEdge()) {
-      r.drawCachedElementPortion(context, ele, slbTxrCache, pxRatio, lvl, reason, getSourceLabelRotation);
-      r.drawCachedElementPortion(context, ele, tlbTxrCache, pxRatio, lvl, reason, getTargetLabelRotation);
+      r.drawCachedElementPortion(context, ele, slbTxrCache, pxRatio, lvl, reason, getSourceLabelRotation, getTextOpacity);
+      r.drawCachedElementPortion(context, ele, tlbTxrCache, pxRatio, lvl, reason, getTargetLabelRotation, getTextOpacity);
     }
 
     r.drawElementOverlay(context, ele);
@@ -27768,7 +27795,7 @@ CRp$2.drawEdge = function (context, edge, shiftToOriginWithBb) {
   var r = this;
   var rs = edge._private.rscratch;
 
-  if (!edge.visible()) {
+  if (shouldDrawOpacity && !edge.visible()) {
     return;
   } // if bezier ctrl pts can not be calculated, then die
 
@@ -28271,7 +28298,7 @@ CRp$4.drawElementText = function (context, ele, shiftToOriginWithBb, force, pref
   var r = this;
 
   if (force == null) {
-    if (!r.eleTextBiggerThanMin(ele)) {
+    if (useEleOpacity && !r.eleTextBiggerThanMin(ele)) {
       return;
     }
   } else if (force === false) {
@@ -28354,7 +28381,7 @@ CRp$4.setupTextStyle = function (context, ele) {
   var labelSize = ele.pstyle('font-size').pfValue + 'px';
   var labelFamily = ele.pstyle('font-family').strValue;
   var labelWeight = ele.pstyle('font-weight').strValue;
-  var opacity = ele.pstyle('text-opacity').value * (useEleOpacity ? ele.effectiveOpacity() : 1);
+  var opacity = useEleOpacity ? ele.effectiveOpacity() * ele.pstyle('text-opacity').value : 1;
   var outlineOpacity = ele.pstyle('text-outline-opacity').value * opacity;
   var color = ele.pstyle('color').value;
   var outlineColor = ele.pstyle('text-outline-color').value;
@@ -28408,7 +28435,7 @@ CRp$4.drawText = function (context, ele, prefix) {
   var rscratch = _p.rscratch;
   var parentOpacity = useEleOpacity ? ele.effectiveOpacity() : 1;
 
-  if (parentOpacity === 0 || ele.pstyle('text-opacity').value === 0) {
+  if (useEleOpacity && (parentOpacity === 0 || ele.pstyle('text-opacity').value === 0)) {
     return;
   } // use 'main' as an alias for the main label (i.e. null prefix)
 
@@ -28648,7 +28675,7 @@ CRp$5.drawNode = function (context, node, shiftToOriginWithBb) {
     return; // can't draw node with undefined position
   }
 
-  if (!node.visible()) {
+  if (shouldDrawOpacity && !node.visible()) {
     return;
   }
 
@@ -30656,7 +30683,7 @@ sheetfn.appendToStyle = function (style) {
   return style;
 };
 
-var version = "3.6.4";
+var version = "3.6.5";
 
 var cytoscape = function cytoscape(options) {
   // if no options specified, use default

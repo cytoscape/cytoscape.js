f07eb1c14d997d02332f01ef5dc20430847c4ea4
Max Franz <maxkfranz@gmail.com>
2019-07-30 14:47:44 -0400

    Improve safety re. bounding box caches for compounds
    - Ensure that `bodyBounds` etc. are nulled on dirtying the bounding box.
    - In `ele.boundingBox()`, make sure that the non-infinity application is made regardless of whether the cache is used.
    - When creating an element, make sure it has an unfilled `arrowBounds` object.
    - Add a test case to help detect issues with display:none children w.r.t. compounds.
    - Add test cases for visibility:hidden and opacity:0.
    
    Ref : Backport 3.7.x : Compound node bounds including hidden children (regression) #2482 #2480

--- test/collection-compound-nodes.js
+++ test/collection-compound-nodes.js
@@ -3,6 +3,17 @@ var cytoscape = require('../src/test.js', cytoscape);
 
 describe('Collection compound nodes', function(){
 
+  var copyBounds = function(bb){
+    return {
+      x1: bb.x1,
+      x2: bb.x2,
+      y1: bb.y1,
+      y2: bb.y2,
+      w: bb.w,
+      h: bb.h
+    };
+  };
+
   var cy, n1, n2, n3, n4;
 
   // test setup
@@ -160,5 +171,71 @@ describe('Collection compound nodes', function(){
     expect( bb2.x2 ).to.equal( bb1.x2 + delta );
     expect( bb2.y1 ).to.equal( bb1.y1 + delta );
     expect( bb2.y2 ).to.equal( bb1.y2 + delta );
+  });
+
+  it('display:none child does reduce parent boundingbox', function(){
+    var bb1 = copyBounds(n2.boundingBox());
+
+    n4.style('display', 'none');
+
+    // n4 should be 0x0
+    var n4bb = n4.boundingBox();
+    var n4bbLabel = n4.boundingBox({ includeNodes: false, includeLabels: true, includeOverlays: false });
+    var n4bBody = n4.boundingBox({ includeNodes: true, includeLabels: false, includeOverlays: false });
+    var n4bbOverlay = n4.boundingBox({ includeNodes: true, includeLabels: false, includeOverlays: true });
+
+    expect(n4bb.w, 'n4 bb w').to.equal(0);
+    expect(n4bb.h, 'n4 bb h').to.equal(0);
+    expect(n4bbLabel.w, 'n4 bb label w').to.equal(0);
+    expect(n4bbLabel.h, 'n4 bb label h').to.equal(0);
+    expect(n4bBody.w, 'n4 bb body w').to.equal(0);
+    expect(n4bBody.h, 'n4 bb body h').to.equal(0);
+    expect(n4bbOverlay.w, 'n4 bb overlay w').to.equal(0);
+    expect(n4bbOverlay.h, 'n4 bb overlay h').to.equal(0);
+
+    var bb2 = copyBounds(n2.boundingBox());
+
+    // sanity check: no dimension should be bigger
+    expect(bb2.w).to.be.lte(bb1.w);
+    expect(bb2.h).to.be.lte(bb1.h);
+
+    // the area should be reduced
+    expect(bb2.w * bb2.h).to.be.lessThan(bb1.w * bb1.h);
+  });
+
+  it('visibility:hidden child does not reduce parent boundingbox', function(){
+    var bb1 = copyBounds(n2.boundingBox());
+
+    n4.style('visibility', 'hidden');
+
+    // n4 should not be 0x0
+    var n4bb = n4.boundingBox();
+
+    expect(n4bb.w, 'n4 bb w').to.not.equal(0);
+    expect(n4bb.h, 'n4 bb h').to.not.equal(0);
+
+    var bb2 = copyBounds(n2.boundingBox());
+
+    // same parent dims
+    expect(bb2.w).to.equal(bb1.w);
+    expect(bb2.h).to.equal(bb1.h);
+  });
+
+  it('opacity:0 child does not reduce parent boundingbox', function(){
+    var bb1 = copyBounds(n2.boundingBox());
+
+    n4.style('opacity', 0);
+
+    // n4 should not be 0x0
+    var n4bb = n4.boundingBox();
+
+    expect(n4bb.w, 'n4 bb w').to.not.equal(0);
+    expect(n4bb.h, 'n4 bb h').to.not.equal(0);
+
+    var bb2 = copyBounds(n2.boundingBox());
+
+    // same parent dims
+    expect(bb2.w).to.equal(bb1.w);
+    expect(bb2.h).to.equal(bb1.h);
   });
 });

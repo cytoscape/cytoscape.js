ad3ea8900d5bb9eef62ac65baf69123c8d7e1c1b
Max Franz <maxkfranz@gmail.com>
2019-09-20 16:00:04 -0400

    Update livereload & config on the debug page

--- debug/livereload-setup.js
+++ debug/livereload-setup.js
@@ -1,9 +1,14 @@
 /* global document, location, window */
 
+// window.LiveReloadOptions = {
+//   host: location.hostname,
+//   port: 35729
+// };
+
 window.addEventListener('DOMContentLoaded', function(){
   var script = document.createElement('script');
 
-  script.src = 'http://' + location.hostname + ':35729/livereload.js';
+  script.src = 'http://' + location.hostname + ':35729/livereload.js?snipver=1';
 
   document.head.appendChild( script );
 });

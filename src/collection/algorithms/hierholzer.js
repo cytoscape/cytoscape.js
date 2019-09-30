c94e113cbb6b62f3fd9a91e3d44a52714d3de08c
r-ba <rhysbalevicius@gmail.com>
2019-09-30 00:50:57 -0400

    Added support for undirected graphs

--- src/collection/algorithms/hierholzer.js
+++ src/collection/algorithms/hierholzer.js
@@ -22,34 +22,54 @@ let elesfn = ({
     if (root) startVertex = root.slice(1); // remove #
     let nodes = {}; // nodeId -> [...edgeIds]
     let edges = {}; // edgeId -> [source, target]
+
     // setup ds for alg and check if not eulerian
-    eles.forEach(function(ele){
-      let id = ele.id();
-      if(ele.isNode()) {
-        let ind = ele.indegree(true);
-        let outd = ele.outdegree(true);
-        let d1 = ind - outd;
-        let d2 = outd - ind;
-        // ensure existence: either all nodes have ind==outd, or
-        // exactly one has ind-outd==1 and exactly one has outd-ind==1
-        if (d1 == 1) {
-          if (oddIn) dflag = true;
-          else oddIn = id; // terminal node
-        } else if (d2 == 1) {
-          if (oddOut) dflag = true;
-          else oddOut = id; // startVertex
-        } else if ((d2 > 1) || (d1 > 1)) {
-          dflag = true;
-        }
-        // collect the ids of each outgoing edge
-        nodes[id] = [];
-        ele.outgoers().forEach(e => {
-          if (e.isEdge()) nodes[id].push(e.id());
-        });
-      } else {
-        edges[id] = ele.target().id();
-      };
-    });
+    if (directed) {
+      eles.forEach(function(ele){
+        let id = ele.id();
+        if(ele.isNode()) {
+          let ind = ele.indegree(true);
+          let outd = ele.outdegree(true);
+          let d1 = ind - outd;
+          let d2 = outd - ind;
+          // ensure existence: either all nodes have ind==outd, or
+          // exactly one has ind-outd==1 and exactly one has outd-ind==1
+          if (d1 == 1) {
+            if (oddIn) dflag = true;
+            else oddIn = id; // terminal node
+          } else if (d2 == 1) {
+            if (oddOut) dflag = true;
+            else oddOut = id; // startVertex
+          } else if ((d2 > 1) || (d1 > 1)) {
+            dflag = true;
+          }
+          // collect the ids of each outgoing edge
+          nodes[id] = [];
+          ele.outgoers().forEach(e => {
+            if (e.isEdge()) nodes[id].push(e.id());
+          });
+        } else {
+          edges[id] = [undefined, ele.target().id()];
+        };
+      });
+    } else {
+      eles.forEach(function(ele){
+        let id = ele.id();
+        if(ele.isNode()) {
+          let d = ele.degree(true);
+          // ensure existence: more than two vertices with odd degree => not eulerian
+          if (d%2) {
+            if (!oddIn) oddIn = id;
+            else if (!oddOut) oddOut = id;
+            else dflag = true;
+          };
+          nodes[id] = [];
+          ele.connectedEdges().forEach(e => nodes[id].push(e.id()));
+        } else {
+          edges[id] = [ele.source().id(), ele.target().id()];
+        };
+      });
+    };
 
     let result = {
       pathExists: false,
@@ -65,28 +85,40 @@ let elesfn = ({
     // if an eulerian circuit exists then any starting node can be used
     if (oddOut && oddIn) {
       result.pathExists = true;
-      // ensure root provided is oddOut
-      if (startVertex && (oddOut != startVertex)) {
-        return result;
-      }
-      startVertex = oddOut;
+      if (directed) {
+        // ensure root provided is oddOut
+        if (startVertex && (oddOut != startVertex)) {
+          return result;
+        };
+        startVertex = oddOut;
+      } else {
+        if (startVertex && (oddOut != startVertex) && (oddIn != startVertex)) {
+          return result;
+        } else if (!startVertex) {
+          startVertex = oddOut;
+        };
+      };
     } else {
       result.circuitExists = true;
       if (!startVertex) startVertex = eles[0].id();
-    }
+    };
 
     const walk = (v) => {
       let currentNode = v;
       let subtour = [v];
-      let adj, adjHead;
+      let adj, adjTail, adjHead;
       while (nodes[currentNode].length) {
         adj = nodes[currentNode].shift();
-        adjHead = edges[adj];
+        adjTail = edges[adj][0];
+        adjHead = edges[adj][1];
         // if adj is not loop, remove other ref to edge
         if (currentNode != adjHead) {
           nodes[adjHead] = nodes[adjHead].filter(e => e != adj);
           currentNode = adjHead;
-        }
+        } else if (!directed && (currentNode != adjTail)) {
+          nodes[adjTail] = nodes[adjTail].filter(e => e != adj);
+          currentNode = adjTail;
+        };
         subtour.unshift(adj);
         subtour.unshift(currentNode);
       }

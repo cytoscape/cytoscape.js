389f1f1113ee7ebdfa558b4b372149079865dcf1
Max Franz <maxkfranz@gmail.com>
2019-11-19 15:53:04 -0500

    Build 3.11.1

--- dist/cytoscape.cjs.js
+++ dist/cytoscape.cjs.js
@@ -9382,7 +9382,12 @@ var cachedBoundingBoxImpl = function cachedBoundingBoxImpl(ele, opts) {
   var currPosKey = getBoundingBoxPosKey(ele);
   var isPosKeySame = _p.bbCachePosKey === currPosKey;
   var useCache = opts.useCache && isPosKeySame;
-  var needRecalc = !useCache || _p.bbCache == null;
+
+  var isDirty = function isDirty(ele) {
+    return ele._private.bbCache == null;
+  };
+
+  var needRecalc = !useCache || isDirty(ele) || isEdge && isDirty(ele.source()) || isDirty(ele.target());
 
   if (needRecalc) {
     if (!isPosKeySame) {
@@ -28012,10 +28017,17 @@ CRp$1.drawCachedElement = function (context, ele, pxRatio, extent, lvl, requestH
   }
 
   if (!extent || boundingBoxesIntersect(bb, extent)) {
-    r.drawCachedElementPortion(context, ele, eleTxrCache, pxRatio, lvl, reason, getZeroRotation, getOpacity);
-    r.drawCachedElementPortion(context, ele, lblTxrCache, pxRatio, lvl, reason, getLabelRotation, getTextOpacity);
+    var isEdge = ele.isEdge();
 
-    if (ele.isEdge()) {
+    var badLine = ele.element()._private.rscratch.badLine;
+
+    r.drawCachedElementPortion(context, ele, eleTxrCache, pxRatio, lvl, reason, getZeroRotation, getOpacity);
+
+    if (!isEdge || !badLine) {
+      r.drawCachedElementPortion(context, ele, lblTxrCache, pxRatio, lvl, reason, getLabelRotation, getTextOpacity);
+    }
+
+    if (isEdge && !badLine) {
       r.drawCachedElementPortion(context, ele, slbTxrCache, pxRatio, lvl, reason, getSourceLabelRotation, getTextOpacity);
       r.drawCachedElementPortion(context, ele, tlbTxrCache, pxRatio, lvl, reason, getTargetLabelRotation, getTextOpacity);
     }
@@ -28608,12 +28620,14 @@ CRp$4.drawElementText = function (context, ele, shiftToOriginWithBb, force, pref
     context.textAlign = justification;
     context.textBaseline = 'bottom';
   } else {
+    var badLine = ele.element()._private.rscratch.badLine;
+
     var _label = ele.pstyle('label');
 
     var srcLabel = ele.pstyle('source-label');
     var tgtLabel = ele.pstyle('target-label');
 
-    if ((!_label || !_label.value) && (!srcLabel || !srcLabel.value) && (!tgtLabel || !tgtLabel.value)) {
+    if (badLine || (!_label || !_label.value) && (!srcLabel || !srcLabel.value) && (!tgtLabel || !tgtLabel.value)) {
       return;
     }
 
@@ -31024,7 +31038,7 @@ sheetfn.appendToStyle = function (style) {
   return style;
 };
 
-var version = "3.11.0";
+var version = "3.11.1";
 
 var cytoscape = function cytoscape(options) {
   // if no options specified, use default

52bc61577f4d75fc5624ae603f13a0b1d2666c15
Max Franz <maxkfranz@gmail.com>
2021-02-17 10:35:46 -0500

    Text event is not triggered when text is rotated #2813

--- src/extensions/renderer/base/coord-ele-math/coords.js
+++ src/extensions/renderer/base/coord-ele-math/coords.js
@@ -252,16 +252,18 @@ BRp.findNearestElements = function( x, y, interactiveElementsOnly, isTouch ){
 
     if( !eventsEnabled || !text ){ return; }
 
-    var rstyle = _p.rstyle;
-    var lx = preprop( rstyle, 'labelX', prefix );
-    var ly = preprop( rstyle, 'labelY', prefix );
+    var lx = preprop( _p.rscratch, 'labelX', prefix );
+    var ly = preprop( _p.rscratch, 'labelY', prefix );
 
     var theta = preprop( _p.rscratch, 'labelAngle', prefix );
 
-    var lx1 = bb.x1 - th;
-    var lx2 = bb.x2 + th;
-    var ly1 = bb.y1 - th;
-    var ly2 = bb.y2 + th;
+    var ox = ele.pstyle(prefixDash + 'text-margin-x').pfValue;
+    let oy = ele.pstyle(prefixDash + 'text-margin-y').pfValue;
+
+    var lx1 = bb.x1 - th - ox; // (-ox, -oy) as bb already includes margin
+    var lx2 = bb.x2 + th - ox; // and rotation is about (lx, ly)
+    var ly1 = bb.y1 - th - oy;
+    var ly2 = bb.y2 + th - oy;
 
     if( theta ){
       var cos = Math.cos( theta );
@@ -282,11 +284,11 @@ BRp.findNearestElements = function( x, y, interactiveElementsOnly, isTouch ){
       var px2y1 = rotate( lx2, ly1 );
       var px2y2 = rotate( lx2, ly2 );
 
-      var points = [
-        px1y1.x, px1y1.y,
-        px2y1.x, px2y1.y,
-        px2y2.x, px2y2.y,
-        px1y2.x, px1y2.y
+      var points = [ // with the margin added after the rotation is applied
+        px1y1.x + ox, px1y1.y + oy,
+        px2y1.x + ox, px2y1.y + oy,
+        px2y2.x + ox, px2y2.y + oy,
+        px1y2.x + ox, px1y2.y + oy
       ];
 
       if( math.pointInsidePolygonPoints( x, y, points ) ){

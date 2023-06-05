2411f508f2fa0556a3d20c812ba3d726ad8130da
Akash Mondal <akmo3901@gmail.com>
2023-06-06 00:13:32 +0530

    Issue #4: Add bash script to fetch new parse string

---
+++ .github/workflows/scripts/new-patch-version.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+entry=$1 #reading expected patch version
+
+curl -L -o Parse-Version-String.js https://raw.githubusercontent.com/AkMo3/cytoscape.js/unstable/.github/workflows/scripts/Parse-Version-String.js
+NEW_PATCH_VERSION=$(node Parse-Version-String.js)
+
+echo "NEW_PATCH_VERSION=$NEW_PATCH_BRANCH" >> $GITHUB_ENV

2063a51c04cb4ad1787b7b8dc2a39bdb9855a798
Akash Mondal <akmo3901@gmail.com>
2023-06-06 00:24:59 +0530

    Update new-patch-version.sh

--- .github/workflows/scripts/new-patch-version.sh
+++ .github/workflows/scripts/new-patch-version.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-entry=$1 #reading expected patch version
-
 curl -L -o Parse-Version-String.js https://raw.githubusercontent.com/AkMo3/cytoscape.js/unstable/.github/workflows/scripts/Parse-Version-String.js
 node Parse-Version-String.js
 NEW_PATCH_VERSION=$(node Parse-Version-String.js)
 
-echo "NEW_PATCH_VERSION=$NEW_PATCH_BRANCH" >> $GITHUB_ENV
+echo "NEW_PATCH_VERSION=$NEW_PATCH_VERSION" >> $GITHUB_ENV

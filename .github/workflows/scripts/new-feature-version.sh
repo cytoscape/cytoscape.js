27d01fb242e8099d8f8aca1a3f22305587b3d430
Akash Mondal <akmo3901@gmail.com>
2023-06-13 00:51:12 +0530

    Fix: Update environment variable to VERSION

--- .github/workflows/scripts/new-feature-version.sh
+++ .github/workflows/scripts/new-feature-version.sh
@@ -3,4 +3,4 @@
 curl -L -o New-Feature-Version.js https://raw.githubusercontent.com/AkMo3/cytoscape.js/unstable/.github/workflows/scripts/New-Feature-Version.js
 NEW_FEATURE_VERSION=$(node New-Feature-Version.js $1)
 echo "New Feature Version $NEW_FEATURE_VERSION"
-echo "NEW_FEATURE_VERSION=$NEW_FEATURE_VERSION" >> $GITHUB_ENV
+echo "VERSION=$NEW_FEATURE_VERSION" >> $GITHUB_ENV

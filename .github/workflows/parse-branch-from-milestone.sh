be5c2f9a04c8a873038284d8b61b6aeae0c5cd1d
Akash Mondal <akmo3901@gmail.com>
2023-08-13 19:15:24 +0530

    Fix: Remove post publish for github action

---
+++ .github/workflows/parse-branch-from-milestone.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+entry=$1 #reading string value  
+      
+IFS='.' #setting comma as delimiter  
+read -a strarr <<<"$entry" #reading str as an array as tokens separated by IFS  
+      
+# echo "maj : ${strarr[0]} "  
+# echo "min : ${strarr[1]} "  
+# echo "build : ${strarr[2]}"  
+IFS=' '
+echo "${strarr[0]}.${strarr[1]}.x"
+echo "PATCH_BRANCH=${strarr[0]}.${strarr[1]}.x" >> $GITHUB_ENV

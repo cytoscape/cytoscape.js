885d380de74787d7c0d0e38ae0379fc07d5a52f1
Akash Mondal <akmo3901@gmail.com>
2023-08-31 15:30:04 +0530

    Fix: Add better next version generation mechanism

--- .github/workflows/scripts/new-patch-version.sh
+++ .github/workflows/scripts/new-patch-version.sh
@@ -4,17 +4,29 @@ PREV_VERSION=$(jq -r '.version' package.json)
 echo "Prev Patch Version $PREV_VERSION"
 
 # Split the version number into major, minor, and patch components
-IFS='.' read -ra VERSION_ARRAY <<< "$PREV_VERSION"
-echo "IFS COMPLETED"
-PATCH_VERSION="${VERSION_ARRAY[2]}"
-echo "CURRENt PATCH VERSION" $PATCH_VERSION
+IFS='.' read -a VERSION_ARRAY <<< "$PREV_VERSION"
+echo "SPLITTING COMPLETED"
 
-# Increment patch for new backport branch
-((PATCH_VERSION++))
-echo "UPDATED PATCH VERSION" $PATCH_VERSION
+major=${VERSION_ARRAY[0]}
+minor=${VERSION_ARRAY[1]}
+patch=${VERSION_ARRAY[2]}
+echo "CURRENT PATCH VERSION" $patch
 
-VERSION="${VERSION_ARRAY[0]}.${VERSION_ARRAY[1]}.${PATCH_VERSION}"
-BRANCH="${VERSION_ARRAY[0]}.${MINOR_VERSION}.x"
+patch=$((patch + 1))
+echo "UPDATED PATCH VERSION" $patch
+
+VERSION="$major.$minor.$patch"
+
+# Split the version number into major, minor, and patch components
+IFS='.' read -a VERSION_ARRAY_2 <<< "$VERSION"
+if [[ ${#VERSION_ARRAY_2[@]} -lt 3 ]]; then
+    echo "Error: Invalid new version format"
+    exit 1
+fi
+
+if [ $BRANCH != "refs/heads/master" ]; then
+    BRANCH="${VERSION_ARRAY[0]}.${VERSION_ARRAY[1]}.x"
+fi
 
 echo "Version $VERSION"
 

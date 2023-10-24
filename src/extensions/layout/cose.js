19fb17857005294b45a49e77dcd45d5981d24edf
Christian Lopes <chrtannus@gmail.com>
2023-10-24 15:46:39 -0300

    Fixes COSE TypeError by filtering the edges in the passed-in options (discards any edge that has a connected node not in the passed-in node collection) -- #3170

--- src/extensions/layout/cose.js
+++ src/extensions/layout/cose.js
@@ -105,8 +105,19 @@ var defaults = {
  */
 function CoseLayout( options ){
   this.options = util.extend( {}, defaults, options );
-
   this.options.layout = this;
+
+  // Exclude any edge that has a source or target node that is not in the set of passed-in nodes
+  const nodes = this.options.eles.nodes();
+  const edges = this.options.eles.edges();
+  const notEdges = edges.filter((e) => {
+    const sourceId = e.source().data('id');
+    const targetId = e.target().data('id');
+    const hasSource = nodes.some((n) => n.data('id') === sourceId);
+    const hasTarget = nodes.some((n) => n.data('id') === targetId);
+    return !hasSource || !hasTarget;
+  });
+  this.options.eles = this.options.eles.not(notEdges);
 }
 
 /**

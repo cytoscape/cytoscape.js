b752d41da39916ec47315caad0899602c2a26964
Max Franz <maxkfranz@gmail.com>
2023-10-26 10:30:23 -0400

    Update debug demo with data for outlines

--- debug/init.js
+++ debug/init.js
@@ -6,52 +6,6 @@ var cy, defaultSty, options;
 (function(){
   defaultSty  = cytoscape.stylesheet()
 
-      .selector('node')
-        .style({
-          'label': 'data(id)'
-        })
-
-      .selector('edge')
-        .style({
-          'source-arrow-shape': 'triangle-backcurve',
-          'target-arrow-shape': 'triangle',
-          'mid-target-arrow-shape': 'triangle',
-          'mid-source-arrow-shape': 'triangle-backcurve'
-        })
-      .selector('#ab')
-        .style({
-          'curve-style': 'unbundled-bezier',
-          'control-point-distances': [ 20, -100, 20 ],
-          'control-point-weights': [ 0.25, 0.5, 0.75 ],
-        })
-      .selector('#bc')
-        .style({
-          'curve-style': 'segments',
-          'segment-distances': [ 20, -80 ],
-          'segment-weights': [ 0.25, 0.5 ],
-        })
-      .selector('#ef')
-        .style({
-          'curve-style': 'straight-triangle',
-          'source-arrow-shape': 'none',
-          'target-arrow-shape': 'none',
-          'mid-target-arrow-shape': 'none',
-          'mid-source-arrow-shape': 'none',
-          'width': 6,
-        })
-      .selector('[source = "c"][target = "e"]')
-        .style({
-          'curve-style': 'haystack',
-          'haystack-radius': 0.5
-        })
-      .selector('[source = "d"][target = "e"]')
-        .style({
-          'curve-style': 'bezier'
-        })
-      .selector('[source = "b"][target = "f"]')
-        .style({
-          'curve-style': 'taxi'
-        })
   ;
 
   options = {
@@ -67,38 +21,148 @@ var cy, defaultSty, options;
       cols: 3
     },
 
-    style: defaultSty,
+    style: [{
+      "selector": "node",
+      "style": {
+        "shape": "data(type)",
+        "label": "data(type)",
+        "height": 40,
+        "width": 40,
+        "outline-opacity": 0.5,
+        "outline-width": 3,
+        "outline-color": "red",
+        "outline-offset": 10
+      }
+    }, {
+      "selector": "node[points]",
+      "style": {
+        "shape-polygon-points": "data(points)",
+        "label": "polygon\n(custom points)",
+        "text-wrap": "wrap"
+      }
+    }]
+    ,
 
-    elements: {
-      nodes: [
-        { data: { id: 'a', weight: 50 } },
-        { data: { id: 'b', weight: 30 } },
-        { data: { id: 'c', weight: 20 } },
-        { data: { id: 'd', weight: 10 } },
-        { data: { id: 'e', weight: 75 } },
-        { data: { id: 'f', weight: 100 } }
-      ],
-
-      edges: [
-        { data: { id: 'ae', weight: 1, source: 'a', target: 'e' } },
-        { data: { id: 'aa', weight: 2, source: 'a', target: 'a' } },
-        { data: { id: 'aa2', weight: 2, source: 'a', target: 'a' } },
-        { data: { id: 'aa3', weight: 2, source: 'a', target: 'a' } },
-        { data: { id: 'ab', weight: 3, source: 'a', target: 'b' } },
-        { data: { id: 'be', weight: 4, source: 'b', target: 'e' } },
-        { data: { id: 'bc', weight: 5, source: 'b', target: 'c' } },
-        { data: { id: 'ce', weight: 6, source: 'c', target: 'e' } },
-        { data: { id: 'ce2', weight: 6, source: 'c', target: 'e' } },
-        { data: { id: 'cd', weight: 2, source: 'c', target: 'd' } },
-        { data: { id: 'de', weight: 7, source: 'd', target: 'e' } },
-        { data: { id: 'de2', weight: 7, source: 'd', target: 'e' } },
-        { data: { id: 'de3', weight: 7, source: 'd', target: 'e' } },
-        { data: { id: 'de4', weight: 7, source: 'd', target: 'e' } },
-        { data: { id: 'de5', weight: 7, source: 'd', target: 'e' } },
-        { data: { id: 'bf', weight: 3, source: 'b', target: 'f' } },
-        { data: { id: 'ef', weight: 3, source: 'e', target: 'f' } }
-      ]
-    }
+    elements: [{
+      "data": {
+        "type": "ellipse"
+      }
+    }, {
+      "data": {
+        "type": "triangle"
+      }
+    }, {
+      "data": {
+        "type": "round-triangle"
+      }
+    }, {
+      "data": {
+        "type": "rectangle"
+      }
+    }, {
+      "data": {
+        "type": "round-rectangle"
+      }
+    }, {
+      "data": {
+        "type": "bottom-round-rectangle"
+      }
+    }, {
+      "data": {
+        "type": "cut-rectangle"
+      }
+    }, {
+      "data": {
+        "type": "barrel"
+      }
+    }, {
+      "data": {
+        "type": "rhomboid"
+      }
+    }, {
+      "data": {
+        "type": "right-rhomboid"
+      }
+    }, {
+      "data": {
+        "type": "diamond"
+      }
+    }, {
+      "data": {
+        "type": "round-diamond"
+      }
+    }, {
+      "data": {
+        "type": "pentagon"
+      }
+    }, {
+      "data": {
+        "type": "round-pentagon"
+      }
+    }, {
+      "data": {
+        "type": "hexagon"
+      }
+    }, {
+      "data": {
+        "type": "round-hexagon"
+      }
+    }, {
+      "data": {
+        "type": "concave-hexagon"
+      }
+    }, {
+      "data": {
+        "type": "heptagon"
+      }
+    }, {
+      "data": {
+        "type": "round-heptagon"
+      }
+    }, {
+      "data": {
+        "type": "octagon"
+      }
+    }, {
+      "data": {
+        "type": "round-octagon"
+      }
+    }, {
+      "data": {
+        "type": "star"
+      }
+    }, {
+      "data": {
+        "type": "tag"
+      }
+    }, {
+      "data": {
+        "type": "round-tag"
+      }
+    }, {
+      "data": {
+        "type": "vee"
+      }
+    }, {
+      "data": {
+        "type": "polygon",
+        "points": [
+          -0.33, -1,
+          0.33, -1,
+          0.33, -0.33,
+          1, -0.33,
+          1, 0.33,
+          0.33, 0.33,
+          0.33, 1,
+          -0.33, 1,
+          -0.33, 0.33,
+          -1, 0.33,
+          -1, -0.33,
+          -0.33, -0.33
+        ]
+      }
+    }]
+    
   };
 
   cy = cytoscape(options);

11d21b8b4bbe42da2e375b5149446f0443b5032c
Max Franz <maxkfranz@gmail.com>
2024-07-04 09:59:24 -0400

    Clean up release script: pre_release_test.sh
    - Enable `set -e`
    - Proper exit statements
    - Fix if
    - Add "dist/cytoscape.umd.js" dist file to check for release
    - Quote variables

--- .github/workflows/scripts/pre_release_test.sh
+++ .github/workflows/scripts/pre_release_test.sh
@@ -1,43 +1,43 @@
 #!/bin/bash
 
 # Make script exit on first failure
-# set -e
+set -e
 
 # Check if VERSION variable is set
 if [ -z "$VERSION" ]; then
   echo "VERSION variable is not set."
-  exit 1;
+  exit 1
 else
   echo "VERSION is set to: $VERSION"
 fi
 
-# Check if current Git branch is named "master"
+# Check if current Git branch is named "master" or the provided branch name
 current_branch=$(git symbolic-ref --short HEAD 2>/dev/null)
 
 if [ "$current_branch" = "$1" ]; then
   echo "Current Git branch is $1."
 else
   echo "Current Git branch is not $1."
-  return 2;
+  exit 2
 fi
 
 FILE=./documentation/versions.json
 if [ -f "$FILE" ]; then
-    echo "$FILE exists."
+  echo "$FILE exists."
 else 
-  echo "$FILE doesn't exists. Exiting..."
+  echo "$FILE doesn't exist. Exiting..."
   exit 1
 fi
-  
+
 npm run release
 
-if [ "$current_branch" = "unstable"] || ["$current_branch" = "master"]; then 
+if [ "$current_branch" = "unstable" ] || [ "$current_branch" = "master" ]; then 
   echo "Starting to check changed files"
 
-  # List the files  to check
-  files_to_check=("documentation/index.html" "documentation/js/cytoscape.min.js" "documentation/versions.json")
+  # List the files to check
+  files_to_check=("documentation/index.html" "documentation/js/cytoscape.min.js" "documentation/versions.json" "dist/cytoscape.umd.js")
 
-  echo "files initialised"
+  echo "Files initialized"
 
   git status
 
@@ -46,13 +46,18 @@ if [ "$current_branch" = "unstable"] || ["$current_branch" = "master"]; then
   do
       echo "Checking $file"
 
-      git status -s $file
+      # Check if the file exists in the repository
+      if ! git ls-files --error-unmatch "$file" &> /dev/null; then
+          echo "The file $file does not exist in the repository."
+          exit 1
+      fi
 
       # Check if the file has changed
       output="$(git status -s $file)"
 
       echo "For $file, $output"
 
+      # Check if the file has changed
       if [ -z "$output" ]; then
           echo "The file $file has not changed."
           exit 1
@@ -66,7 +71,7 @@ git add . && git commit -m "Build $VERSION"
 
 git log -n 1
 
-npm version $VERSION --allow-same-version
+npm version "$VERSION" --allow-same-version
 
 git push && git push --tags
 
